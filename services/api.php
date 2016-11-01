3b4d99489d6810fafc692304ddf50c4840c11645
Sergio Alexis Zapata Mejia <szapame@epmtelco.com.co>
2016-11-01 09:41:15 -0500

    cambio

--- services/api.php
+++ services/api.php
@@ -8577,11 +8577,7 @@ private function listadoTransaccionesActividades(){
 	
 
 					       } 
-					      if($h=="M"){
-			                    $timestamp = PHPExcel_Shared_Date::ExcelToPHP($cellValue);//fecha larga
-								$FECHA_INGRESO = gmdate("Y-m-d",$timestamp);//fecha formateada+
-							  	$table .= "<td>";
-							  	}
+
 
                        		
                         if ($tname1 == "IMPORTANTES.xlsx"){
