f3598c0962656a4c9910fb4dc5edce510aba2bc6
Mauricio Acevedo <mauricio.acevedo@une.com.co>
2016-11-03 14:28:32 -0500

    Se cambia el exporte de historicos de agendamiento para que use unbuffered query

--- services/api.php
+++ services/api.php
@@ -310,9 +310,12 @@
                                "  where fecha_fin between '$fechaIni 00:00:00' and '$fechaFin 23:59:59' ";
 
 
-                        $r = $this->mysqli->query($query) or die($this->mysqli->error.__LINE__);
+                        //$r = $this->mysqli->query($query) or die($this->mysqli->error.__LINE__);
 
-                        if($r->num_rows > 0){
+                        //Mauricio: CONSULTA REPODEROSA, unbuffered
+                        $this->mysqli->real_query($query) or die($conn->error.__LINE__);
+
+                        if($r = $connm->use_result()){
                                 $result = array();
                                 $fp = fopen("../tmp/$filename", 'w');
 
