2d18fca6820b53efb6e696d8097d3d2c2fc88a86
Carlos Andres Gonzalez <cgonzgo@epmtelco.com.co>
2016-11-04 07:16:23 -0500

    Se agrega filtro TimeAgo

--- app/app.js
+++ app/app.js
@@ -11388,6 +11388,80 @@ app.directive('tooltip', function() {
    }
 });
 
+// Filtros ------------------------------------------
+
+app.filter('timeago', function() {
+        return function(input, p_allowFuture) {
+
+            var substitute = function (stringOrFunction, number, strings) {
+                    var string = angular.isFunction(stringOrFunction) ? stringOrFunction(number, dateDifference) : stringOrFunction;
+                    var value = (strings.numbers && strings.numbers[number]) || number;
+                    return string.replace(/%d/i, value);
+                },
+                nowTime = (new Date()).getTime(),
+                date = (new Date(input)).getTime(),
+                //refreshMillis= 6e4, //A minute
+                allowFuture = p_allowFuture || false,
+                strings= {
+                    prefixAgo: 'hace',
+                    prefixFromNow: '',
+                    suffixAgo: "",
+                    suffixFromNow: "ahora",
+                    seconds: "unos segs",
+                    minute: "un min",
+                    minutes: "%d mins",
+                    hour: "una hora",
+                    hours: " %d horas",
+                    day: "un dia",
+                    days: "%d dias",
+                    month: "un mes",
+                    months: "%d meses",
+                    year: "un ano",
+                    years: "%d anos"
+                },
+                dateDifference = nowTime - date,
+                words,
+                seconds = Math.abs(dateDifference) / 1000,
+                minutes = seconds / 60,
+                hours = minutes / 60,
+                days = hours / 24,
+                years = days / 365,
+                separator = strings.wordSeparator === undefined ?  " " : strings.wordSeparator,
+
+
+                prefix = strings.prefixAgo,
+                suffix = strings.suffixAgo;
+
+            if (allowFuture) {
+                if (dateDifference < 0) {
+                    prefix = strings.prefixFromNow;
+                    suffix = strings.suffixFromNow;
+                }
+            }
+
+            words = seconds < 45 && substitute(strings.seconds, Math.round(seconds), strings) ||
+            seconds < 90 && substitute(strings.minute, 1, strings) ||
+            minutes < 45 && substitute(strings.minutes, Math.round(minutes), strings) ||
+            minutes < 90 && substitute(strings.hour, 1, strings) ||
+            hours < 24 && substitute(strings.hours, Math.round(hours), strings) ||
+            hours < 42 && substitute(strings.day, 1, strings) ||
+            days < 30 && substitute(strings.days, Math.round(days), strings) ||
+            days < 45 && substitute(strings.month, 1, strings) ||
+            days < 365 && substitute(strings.months, Math.round(days / 30), strings) ||
+            years < 1.5 && substitute(strings.year, 1, strings) ||
+            substitute(strings.years, Math.round(years), strings);
+      //console.log(prefix+words+suffix+separator);
+      prefix.replace(/ /g, '')
+      words.replace(/ /g, '')
+      suffix.replace(/ /g, '')
+      return (prefix+' '+words+' '+suffix+' '+separator);
+
+        };
+    });
+
+//  ------------------------------------------ Filtros
+
+
 app.config( [
     '$compileProvider',
     function( $compileProvider )
