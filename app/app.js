8c0b0cb4fc6d2ad70df390bb75592a95e62da49f
Sergio Alexis Zapata Mejia <szapame@epmtelco.com.co>
2016-11-04 09:56:23 -0500

    cambio

--- app/app.js
+++ app/app.js
@@ -8571,9 +8571,9 @@ $scope.start = function(pedido) {
     $scope.set_color = function (service) {
 
 
-        if (service.PROCESO=='INSTALACION' && service.TODAY_TRIES >= 5) {
+        if (service.proceso=='INSTALACION' && service.TODAY_TRIES >= 5) {
             $scope.error='Recuerde agendar o en su defecto anular el pedido';
-            //alert('pedido especial');
+            alert('pedido especial');
 
                 $scope.estilo={
                  "color" : "white",
@@ -8586,7 +8586,7 @@ $scope.start = function(pedido) {
               }
               else{
 
-                if(service.PROCESO=='REPARACION' && service.TODAY_TRIES >= 3){
+                if(service.proceso=='REPARACION' && service.TODAY_TRIES >= 3){
                 $scope.error='Recuerde agendar o en su defecto cerrar y crear SS';
 
                     $scope.estilo={
