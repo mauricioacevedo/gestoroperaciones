1f0da5a9313c0bd4f87ab0aa57789923dee12c8f
Sergio Alexis Zapata Mejia <szapame@epmtelco.com.co>
2016-11-17 14:53:30 -0500

    cambio

--- services/api.php
+++ services/api.php
@@ -8818,43 +8818,6 @@ private function listadoTransaccionesActividades(){
                       $rrr = $this->mysqli->query($sqlfeed) or die($this->mysqli->error.__LINE__);
 
 
-            //cargamos el archivo
-            $lineas = file($target_file);
-
-            //inicializamos variable a 0, esto nos ayudará a indicarle que no lea la primera línea
-            $i=0;
-
-            //Recorremos el bucle para leer línea por línea
-            foreach ($lineas as $linea_num => $linea)
-            {
-               //abrimos bucle
-               /*si es diferente a 0 significa que no se encuentra en la primera línea
-               (con los títulos de las columnas) y por lo tanto puede leerla*/
-               if($i != 0)
-               {
-                   //abrimos condición, solo entrará en la condición a partir de la segunda pasada del bucle.
-                   /* La funcion explode nos ayuda a delimitar los campos, por lo tanto irá
-                   leyendo hasta que encuentre un ; */
-                   $datos = explode(";",$linea);
-
-                   //Almacenamos los datos que vamos leyendo en una variable
-                   $nombre = trim($datos[0]);
-                   $edad = trim($datos[1]);
-                   $profesion = trim($datos[2]);
-
-                   //guardamos en base de datos la línea leida
-                 $sqlemail="insert into portalbd.gestor_pendientes_reagendamiento (PEDIDO_ID) values ('$PEDIDO_ID'') ";
-					      // echo($sqlemail);
-					       	$r = $this->mysqli->query($sqlemail) or die($this->mysqli->error.__LINE__);
-
-                   //cerramos condición
-               }
-
-               /*Cuando pase la primera pasada se incrementará nuestro valor y a la siguiente pasada ya
-               entraremos en la condición, de esta manera conseguimos que no lea la primera línea.*/
-               $i++;
-               //cerramos bucle
-
          }
 
 
