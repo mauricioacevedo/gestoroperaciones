4a4afb8dde6563df0ee898ada1677542804a970c
Carlos Andres Gonzalez <cgonzgo@epmtelco.com.co>
2016-11-18 11:13:03 -0500

    aut api

--- app/app.js
+++ app/app.js
@@ -3278,7 +3278,7 @@ $scope.ChatRoomMates = function() {
 	$scope.token="ty2p9C36vBqCEU7L";
 
     $http.post($scope.url, {
-			headers: {'Authorization': 'Basic '+$scope.token,
+			headers: {'Authorization': 'Basic YWRtaW46MTIzNDU=',
 					  'Content-Type': 'application/json'}}).
             then(function successCallback(response) {
 
