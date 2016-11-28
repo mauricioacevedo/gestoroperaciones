37f3e8cbbd7ba3094faa8d5a1e7edf2ac0a980ce
Carlos Andres Gonzalez <cgonzgo@epmtelco.com.co>
2016-11-28 16:27:30 -0500

    capitalize

--- app/app.js
+++ app/app.js
@@ -12292,6 +12292,26 @@ app.directive('tooltip', function() {
    }
 });
 
+//Directiva para poner los textos de los input en mayusculas
+ app.directive('capitalize', function() {
+    return {
+      require: 'ngModel',
+      link: function(scope, element, attrs, modelCtrl) {
+        var capitalize = function(inputValue) {
+          if (inputValue == undefined) inputValue = '';
+          var capitalized = inputValue.toUpperCase();
+          if (capitalized !== inputValue) {
+            modelCtrl.$setViewValue(capitalized);
+            modelCtrl.$render();
+          }
+          return capitalized;
+        }
+        modelCtrl.$parsers.push(capitalize);
+        capitalize(scope[attrs.ngModel]); // capitalize initial value
+      }
+    };
+  });
+
 // Filtros ------------------------------------------
 
 app.filter('timeago', function() {
