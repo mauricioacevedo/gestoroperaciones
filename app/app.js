1b9d7f2402cc1c9694c6294631c840eb5658b8b0
Carlos Andres Gonzalez <cgonzgo@epmtelco.com.co>
2016-11-28 16:02:33 -0500

    loca

--- app/app.js
+++ app/app.js
@@ -12111,7 +12111,7 @@ $scope.buscarCliente=function(localidad,direccion){
 
 			$scope.eda.localidad=localidad;
 
-			//console.log($scope.eda.localidad);
+			console.log($scope.eda.localidad);
 
 			if($scope.eda.localidad=='undefined'){
 				alert("Seleccione localidad");
