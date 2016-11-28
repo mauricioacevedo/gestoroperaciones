c7babea3d31e0350d3848823a53a9072c050f2a1
Carlos Andres Gonzalez <cgonzgo@epmtelco.com.co>
2016-11-28 14:28:32 -0500

    otra vez localidad

--- app/app.js
+++ app/app.js
@@ -12081,7 +12081,7 @@ app.controller('edatelCtrl', function ($scope, $rootScope, $location, $routePara
 	// --------------------------------------------------------------- BuscarPedido
 
 	// GuardarPedido --------------------------------------------------------------
-	$scope.edatel=[];
+	$scope.edatel={};
 	$scope.listarLocalidades=function(){
 
 			//$scope.localidad="MEDELLIN";
@@ -12090,7 +12090,7 @@ app.controller('edatelCtrl', function ($scope, $rootScope, $location, $routePara
 			  function(data){
 
 				  $scope.listadoLocalidad=data.data;
-				  $scope.edatel.localidad='MEDELLIN';
+				  $scope.edatel.localidad="MEDELLIN";
 				  return data.data;
 
 
