9096f973d6b970d914ba75846d800faa48a74fa3
Carlos Andres Gonzalez <cgonzgo@epmtelco.com.co>
2016-11-28 16:35:48 -0500

    DIrectiva pal Enter

--- app/app.js
+++ app/app.js
@@ -12313,6 +12313,24 @@ app.directive('tooltip', function() {
     };
   });
 
+app..directive('onKeyEnter', ['$parse', function($parse) {
+        return {
+            restrict: 'A',
+            link: function(scope, element, attrs) {
+                element.bind('keydown keypress', function(event) {
+                    if (event.which === 13) {
+                        var attrValue = $parse(attrs.onKeyEnter);
+                        (typeof attrValue === 'function') ? attrValue(scope) : angular.noop();
+                        event.preventDefault();
+                    }
+                });
+                scope.$on('$destroy', function() {
+                    element.unbind('keydown keypress')
+                })
+            }
+        };
+    }]);
+
 // Filtros ------------------------------------------
 
 app.filter('timeago', function() {
