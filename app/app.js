3b366b091085d689408ed4360feba52cfcce4d76
Carlos Andres Gonzalez <cgonzgo@epmtelco.com.co>
2016-11-29 16:47:35 -0500

    scope local

--- app/app.js
+++ app/app.js
@@ -12172,8 +12172,8 @@ app.controller('edatelCtrl', function ($scope, $rootScope, $location, $routePara
 
 			  function(data){
 				  //console.log(data);
-				  $scope.listadoLocalidad=data.data[0].LOCALIDAD;
-				  console.log($scope.listadoLocalidad);
+				  $scope.listadoLocalidad=data.data[0];
+				  console.log($scope.listadoLocalidad.LOCALIDAD);
 				  return data.data;
 
 
