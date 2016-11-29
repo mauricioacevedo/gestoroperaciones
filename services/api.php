bede38596ca8ce18b4f216e3275717cc363f3418
Carlos Andres Gonzalez <cgonzgo@epmtelco.com.co>
2016-11-29 16:23:06 -0500

    api

--- services/api.php
+++ services/api.php
@@ -11473,7 +11473,7 @@ $sqlfenix=
 								$result[] = $row;
 						}
 
-						$this->response($this->json(($result)), 200); // send user details
+						$this->response($this->json($result), 200); // send user details
 				}
 				$this->response('',406);        // If no records "No Content" status
 
