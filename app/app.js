d759b348bc236adc97da85805d1fc305e632eae4
Sergio Alexis Zapata Mejia <szapame@epmtelco.com.co>
2016-12-01 14:58:44 -0500

    cambio

--- app/app.js
+++ app/app.js
@@ -13053,6 +13053,22 @@ app.run(['$rootScope', 'services', function($rootScope, services){
     	];
 
 	// ------------------------------------------- Listados Siebel
+     // -------------------Listados activacion-------------------------------------------
+
+        $rootScope.transaccionActivacion=[
+            {TRANSACCION:'SUSPENDER', VALOR:'SUSPENDER'},
+            {TRANSACCION:'ELIMINAR', VALOR:'ELIMINAR'},
+            {TRANSACCION:'DISENO', VALOR:'DISENO'},
+            {TRANSACCION:'ELIMINAR COMUNES', VALOR:'ELIMINAR COMUNES'}
+            {TRANSACCION:'NUEVO', VALOR:'NUEVO'},
+            {TRANSACCION:'ACTUALIZAR', VALOR:'ACTUALIZAR'},
+            {TRANSACCION:'ACTUALIZAR COMUNES', VALOR:'ACTUALIZAR COMUNES'}
+            {TRANSACCION:'TRASLADO', VALOR:'TRASLADO'},
+            {TRANSACCION:'ACTUALIZAR', VALOR:'ACTUALIZAR'},
+            {TRANSACCION:'TRASLADO RETIRO', VALOR:'TRASLADO RETIRO'}
+        ];
+
+    // ------------------------------------------- Listados activacion--------------
 
 
 
