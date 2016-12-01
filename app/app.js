f40004219d02c37c30528fd86450705280814d6b
Sergio Alexis Zapata Mejia <szapame@epmtelco.com.co>
2016-12-01 14:03:30 -0500

    cambio

--- app/app.js
+++ app/app.js
@@ -10256,6 +10256,7 @@ app.controller('siebelActivacionCtrl', function ($scope, $rootScope, $location, 
         $scope.busy="";
         $scope.pedido1=pedido1;
         $scope.error="";
+        $scope.TIPIFICACION="";
 
 
         var demePedidoButton=document.getElementById("iniciar");
