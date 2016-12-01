fa5ab3351a7d9fe1677b3cfe2cf437ce91fa06e1
Carlos Andres Gonzalez <cgonzgo@epmtelco.com.co>
2016-12-01 14:23:06 -0500

    no f

--- app/app.js
+++ app/app.js
@@ -1,4 +1,4 @@
-var app = angular.module('myApp', ['base64','ngRoute','ngCookies','ng-fusioncharts','ngAnimate','ui.bootstrap','ui.tinymce','ui.select','ngSanitize','ui.calendar','angularFileUpload','converse']);
+var app = angular.module('myApp', ['base64','ngRoute','ngCookies','ng-fusioncharts','ngAnimate','ui.bootstrap','ui.tinymce','ui.select','ngSanitize','ui.calendar','angularFileUpload']);
 //Los " Myapp " solapas de parámetros a un elemento HTML en el que se ejecutará la aplicación .
 //Ahora puede agregar controladores , directivas , filtros y más, para su aplicación AngularJS .
 //El módulo ngRoute proporciona enrutamiento y deeplinking Servicios y directivas para aplicaciones angulares .
