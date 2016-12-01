8406b70ab9247cfbfcccd79bab0ebaecd8a5d848
Carlos Andres Gonzalez <cgonzgo@epmtelco.com.co>
2016-12-01 10:35:33 -0500

    order

--- services/api.php
+++ services/api.php
@@ -11560,7 +11560,7 @@ $sqlfenix=
 			$today = date("Y-m-d");
 
 
-			$query=	" SELECT DEPARTAMENTO, LOCALIDAD, REGION FROM gestor_informes.eda_localidades ";
+			$query=	" SELECT DEPARTAMENTO, LOCALIDAD, REGION FROM gestor_informes.eda_localidades order by 2 asc";
 					//" where e.LOCALIDAD='$localidad' ";
 
 													//echo $query;
