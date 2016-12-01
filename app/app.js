ce6c29d953a6a0fce38f7d2e9250342e25625301
Sergio Alexis Zapata Mejia <szapame@epmtelco.com.co>
2016-12-01 14:04:10 -0500

    cambio

--- app/app.js
+++ app/app.js
@@ -10262,7 +10262,6 @@ app.controller('siebelActivacionCtrl', function ($scope, $rootScope, $location, 
         var demePedidoButton=document.getElementById("iniciar");
             demePedidoButton.setAttribute("disabled","disabled");
             demePedidoButton.className = "btn btn-success btn-DemePedido-xs disabled";
-            $scope.TIPIFICACION=$scope.peds[0].TIPIFICACION;
         var kami=services.demePedidoActivacion($rootScope.logedUser.login,$rootScope.logedUser.name,'',$scope.pedido1).then(function(data){
 
             $scope.peds = data.data;
