7179da60e4d92f7939e529c1f9555960f19da5f5
Sergio Alexis Zapata Mejia <szapame@epmtelco.com.co>
2016-12-05 09:11:55 -0500

    cambio

--- app/app.js
+++ app/app.js
@@ -10398,7 +10398,7 @@ app.controller('siebelActivacionCtrl', function ($scope, $rootScope, $location, 
 			ASESOR:$rootScope.logedUser.login,
             FECHA_GESTION:$scope.peds[0].FECHA_GESTION,
             STATUS:$scope.peds[0].STATUS,
-
+            TIPIFICACION:$scope.peds[0].TIPIFICACION,
 
 			}
 
