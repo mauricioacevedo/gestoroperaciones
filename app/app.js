29df98fca92844f9695a982b770fae611cdf7b32
Sergio Alexis Zapata Mejia <szapame@epmtelco.com.co>
2016-12-06 16:32:32 -0500

    cambio

--- app/app.js
+++ app/app.js
@@ -431,6 +431,7 @@ app.factory("services", ['$http', '$timeout', function($http,$q,$timeout) {
         }
 
          obj.insertTransaccionsiebelactivacion  = function (pedido) {
+                console.log("pedido="+pedido+", pedido_actual="+pedido_actual+"tipificacion="tipificacion);
                 var data= $http.post(serviceBase + 'insertTransaccionsiebelactivacion ', {"pedido":pedido});
                 return data;
         };
