04743ecbc6cd5d435612802f3a00a27b43eefcb0
Carlos Andres Gonzalez <cgonzgo@epmtelco.com.co>
2016-12-14 13:04:10 -0500

    busca link

--- app/app.js
+++ app/app.js
@@ -13296,7 +13296,7 @@ app.run(function($rootScope, $compile, $window, services) {
 		window.open(window.location.pathname+"#/tips/", "_blank", "toolbar=yes, scrollbars=yes, resizable=yes, top=50, left=20, width=800px, height=600");
 	}
 
-	$rootScope.BuscaRedEdatel=function(){
+	$rootScope.BuscaRedEdatel=function(localidad){
 		window.open(window.location.pathname+"#/distriedatel/", "_blank", "toolbar=yes, scrollbars=yes, resizable=yes, top=50, left=20, width=800px, height=600");
 	}
 
