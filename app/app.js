4b4ad6248b3ec3d369c502a6667b3be1c4123181
Carlos Andres Gonzalez <cgonzgo@epmtelco.com.co>
2016-12-19 16:22:20 -0500

    reconfig

--- app/app.js
+++ app/app.js
@@ -5315,7 +5315,7 @@ $scope.intervalLightKPIS = setInterval(function(){
                 var counter=$rootScope.lightkpi[i].COUNTER;
                 var concepto_id=$rootScope.lightkpi[i].CONCEPTO_ID;
 
-                if(concepto_id=='PETEC'||concepto_id=='OKRED'||concepto_id=='PETEC-BOG'||concepto_id=='PEOPP'||concepto_id=='19'||concepto_id=='O-13'||concepto_id=='O-15'||concepto_id=='O-106'){
+                if(concepto_id=='PETEC'||concepto_id=='OKRED'||concepto_id=='PETEC-BOG'||concepto_id=='PEOPP'||concepto_id=='19'||concepto_id=='O-13'||concepto_id=='O-15'||concepto_id=='O-106'||concepto_id=='COBERTURA'||concepto_id=='CONSTRUCCION'||concepto_id=='DISENO'||concepto_id=='DISPONIBILIDAD'){
                     negocioAsingaciones+="<tr><td><a href='./#/registros/"+concepto_id+"'>"+concepto_id+"</a></td><td>"+counter+"<font color='DarkGray'><strong><i>&nbsp;&nbsp; Servicios</strong></i></font></td></tr>";
                     $rootScope.totalNegocioAsignaciones=parseInt($rootScope.totalNegocioAsignaciones)+parseInt(counter);
                 }else if(concepto_id=='14'||concepto_id=='99'||concepto_id=='92'){
