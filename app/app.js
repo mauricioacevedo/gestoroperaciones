f25fdeeff1009ca4073230a15536096f904491d6
Sergio Alexis Zapata Mejia <szapame@epmtelco.com.co>
2016-12-19 11:42:55 -0500

    cambio

--- app/app.js
+++ app/app.js
@@ -420,7 +420,7 @@ app.factory("services", ['$http', '$timeout', function($http,$q,$timeout) {
          obj.getpedidosPorPedidoActivacion = function(pedido){//pedido por pedido activacion
                 return $http.get(serviceBase + 'pedidosPorPedidoActivacion?pedido=' + pedido);
         }
-        obj.demePedidoActivacion = function(user,pedido_actual,pedido,transaccion){//deme pedido activacion
+        obj.demePedidoActivacion = function(user,pedido,transaccion){//deme pedido activacion
             //console.log("user="+user+",pedido="+pedido);
 		return $http.get(serviceBase+'demePedidoActivacion?pedidoID='+pedido+'&pedido_actual='+pedido_actual+ '&userID='+user+ '&transaccion='+transaccion);
     	}
