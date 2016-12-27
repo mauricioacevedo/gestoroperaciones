538ae6de421cc598612e01297bde62a12341959d
Carlos Andres Gonzalez <cgonzgo@epmtelco.com.co>
2016-12-27 09:18:02 -0500

    val

--- app/app.js
+++ app/app.js
@@ -12642,7 +12642,7 @@ app.controller('chatioCtrl', function ($scope,$route, $rootScope, $location, $ro
 
 
 		listRef.on('value', function(snap) {
-			console.log(snap.val);
+			console.log(snap.val());
 			if(snap.val()===true){
 				$scope.userOnLine=snap.numChildren();
 			}
