afced52fd3e6d88865d4362916dbd717a86fa55e
Carlos Andres Gonzalez <cgonzgo@epmtelco.com.co>
2016-12-28 07:36:03 -0500

    obs

--- app/app.js
+++ app/app.js
@@ -12656,7 +12656,7 @@ app.controller('chatioCtrl', function ($scope,$route, $rootScope, $location, $ro
 
 		//var ref = new Firebase(URL); // assume value here is { foo: "bar" }
 		var obj =   $firebaseArray(listRef);
-		console.log(obj);
+		console.log(obj[0]);
 
 		/*obj.$loaded().then(function() {
         //console.log("loaded record:", obj.$id, obj.someOtherKeyInData);
@@ -12671,8 +12671,8 @@ app.controller('chatioCtrl', function ($scope,$route, $rootScope, $location, $ro
      });*/
 
      // To make the data available in the DOM, assign it to $scope
-     $scope.datica = obj;
-		console.log($scope.datica.$id);
+     //$scope.datica = obj;
+		//console.log($scope.datica.$id);
 
      // For three-way data bindings, bind it to the scope instead
      //obj.$bindTo($scope, "datica");
