3c714e9f8e1e305eddd6a547ab7f828831b8db31
Carlos Andres Gonzalez <cgonzgo@epmtelco.com.co>
2016-12-28 08:07:44 -0500

    pipover

--- app/app.js
+++ app/app.js
@@ -12848,6 +12848,21 @@ app.directive('popover', function() {
    }
 });
 
+app.directive('pipover', function($compile, $timeout){
+  return {
+    restrict: 'A',
+    link:function(scope, el, attrs){
+      var content = attrs.content; //get the template from the attribute
+      var elm = angular.element('<div />'); //create a temporary element
+      elm.append(attrs.content); //append the content
+      $compile(elm)(scope); //compile
+      $timeout(function() { //Once That is rendered
+        el.removeAttr('popover').attr('data-content',elm.html()); //Update the attribute
+        el.popover(); //set up popover
+       });
+    }
+  }
+})
 
 
 app.directive('tooltip', function() {
