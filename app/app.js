572b424b4e5af5e69cd76ccbcec62f98b22267bd
Carlos Andres Gonzalez <cgonzgo@epmtelco.com.co>
2016-12-29 16:27:23 -0500

    prueba cambio de controlador

--- app/app.js
+++ app/app.js
@@ -13805,12 +13805,31 @@ $rootScope.ProgramadosModal=function(){
 
 });
 
-app.run(['$location', '$rootScope', function($location, $rootScope) {
+app.run(['$location', '$rootScope', function($location, $rootScope, $cookies,$cookieStore,$firebase,$firebaseObject,$firebaseArray) {
 
     $rootScope.$on('$routeChangeSuccess', function (event, current, previous) {
         //console.log($rootScope.loginexito);
         $rootScope.title = current.$$route.title;
 		console.log(current.$$route.controller);
+
+		var controlador=current..$$route.controller;
+		var userID=$cookieStore.get('logedUser').login;
+		var root = firebase.database().ref(); // hace refencia a la tabla donde se almacenan los datos
+		var messageRef = $firebaseArray(root.child('messages'));
+		var mensajes = root.child('messages');
+		var listRef = firebase.database().ref('presence');
+		var amOnline = firebase.database().ref('.info/connected');
+		var userRef = firebase.database().ref('presence/' + userID);
+
+		if(controlador!='IndicadoresCtrl'||controlador!='chatioCtrl'){
+			amOnline.on('value', function(snapshot) {
+			  	if (snapshot.val()) {
+				userRef.set(false);
+				userRef.onDisconnect().remove();
+			  }
+			});
+
+		}
 
     });
 
