e35d75edd3d5d53a45bafb60c392c1a1e7cefe3f
Carlos Andres Gonzalez <cgonzgo@epmtelco.com.co>
2017-01-02 08:48:23 -0500

    as

--- app/app.js
+++ app/app.js
@@ -13877,7 +13877,7 @@ app.run(['$location', '$rootScope', '$cookies','$cookieStore','$firebase','$fire
 
     $rootScope.$on('$routeChangeSuccess', function (event, current, previous) {
         //console.log($rootScope.loginexito);
-        $rootScope.title = 'Gestor Operaciones - '+current.$$route.title;
+        $rootScope.title = current.$$route.title;
 		//console.log(current.$$route.controller);
 
 
