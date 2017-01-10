fde9582d04c4c2e0d9c254eaef6031c0a997c9cd
Carlos Andres Gonzalez <cgonzgo@epmtelco.com.co>
2017-01-10 06:49:43 -0500

    cec

--- services/api.php
+++ services/api.php
@@ -2883,6 +2883,8 @@ private function updateFenixReconfiguracion($obj){
 				$filtro= " and $campo = '$valorCampo' and user = '$userID'";
 			}
 			$page=$page*100;
+
+			echo $filtro;
 			//counter
 			$query="SELECT count(*) as counter from pedidos where fecha_fin between '$fechaini 00:00:00' and '$fechafin 23:59:59' $filtro";
 			$rr = $this->mysqli->query($query) or die($this->mysqli->error.__LINE__);
@@ -2896,7 +2898,7 @@ private function updateFenixReconfiguracion($obj){
 				
 			$query="SELECT id, pedido, fuente, actividad, fecha_fin, estado,duracion,accion,concepto_final,user from pedidos where fecha_fin between '$fechaini 00:00:00' and '$fechafin 23:59:59' $filtro order by fecha_fin desc limit 100 offset $page";
 
-					 echo $query;
+					 //echo $query;
                        $r = $this->mysqli->query($query) or die($this->mysqli->error.__LINE__);
 
                         if($r->num_rows > 0){
