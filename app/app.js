cf972bca2e896098f17c4a28f18fc1e0ab4ff7da
Carlos Andres Gonzalez <cgonzgo@epmtelco.com.co>
2017-01-17 07:31:03 -0500

    pedido

--- app/app.js
+++ app/app.js
@@ -6519,9 +6519,9 @@ app.controller('AsignacionesCtrl', function ($scope, $rootScope, $location, $rou
 
 					$scope.pedido.concepto_final = $scope.pedido.con_fenix;
 					//$scope.pedidos = $scope.pedidos.concat($scope.pedido);
-					console.log($scope.pedido);
-					console.log($scope.pedidos);
-					$scope.pedidos = $scope.pedidos.extend($scope.pedido);
+					//console.log($scope.pedido);
+					//console.log($scope.pedidos);
+					$scope.pedidos = $scope.pedido.extend($scope.pedido);
 					if ($scope.historico_pedido == "") {
 						$scope.historico_pedido = new Array();
 					}
