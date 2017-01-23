c8a402350c7838cd0ab837d5c475833d0c4c9810
cgonzgo <cgonzgo@asesor.une.com.co>
2017-01-23 08:45:35 -0500

    Opciones nuevas en Asignaciones #6

--- app/app.js
+++ app/app.js
@@ -6510,7 +6510,7 @@ app.controller('AsignacionesCtrl', function ($scope, $rootScope, $location, $rou
 
 		//$scope.pedido=$scope.peds[index];
 		angular.copy($scope.peds[index], $scope.pedido);
-		//console.log($scope.pedido);
+		console.log($scope.pedido);
 		//if($scope.pedido.estado===undefined||$scope.pedido.accion===undefined){
 		if ($scope.pedido.estado === undefined) {
 			alert('Por favor diligenciar todos los campos.');
