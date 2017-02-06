96f55d143bc8552df82552579588c5b7b521e7ad
cgonzgo <cgonzgo@asesor.une.com.co>
2017-02-06 07:34:31 -0500

    Tabla pendientes por rango horas actualizada

--- services/api.php
+++ services/api.php
@@ -13276,7 +13276,7 @@ class API extends REST {
 
                 $ii++;
                 $sepp=",";
-                if($ii % 1000 == 0){
+                if($ii % 1300 == 0){
                     echo "Carga: $ii<br>";
                     $subinsert="insert into go_agen_ocupacionmicrozonas ($fields) values $subinsert";
                     $this->mysqli->query($subinsert);
