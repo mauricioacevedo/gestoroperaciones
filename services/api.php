815f4012fd7843f3a173ffa9c59a746f02ea12ea
cgonzgo <cgonzgo@asesor.une.com.co>
2017-02-20 13:40:30 -0500

    Feed mejorado

--- services/api.php
+++ services/api.php
@@ -7823,8 +7823,8 @@ class API extends REST {
         }else if($concepto=="14B2B"){
             $concepto=" and b.CONCEPTO_ID='$concepto' and ( b.UEN_CALCULADA !='HG' ) ";
         }else{
-            $concepto=" and b.CONCEPTO_ID='$concepto' and b.TIPO_ELEMENTO_ID IN('ACCESP','INSIP','INSHFC','TO','TOIP','STBOX','EQURED')";
-            //$concepto=" and b.CONCEPTO_ID='$concepto' ";
+            //$concepto=" and b.CONCEPTO_ID='$concepto' and b.TIPO_ELEMENTO_ID IN('ACCESP','INSIP','INSHFC','TO','TOIP','STBOX','EQURED')";
+            $concepto=" and b.CONCEPTO_ID='$concepto' ";
         }
 
         if($plaza=="TODOS"){//para que sea posible obtener un registro de cualquier plaza
