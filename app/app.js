098da0645bb3eaa40c30d4548e7284d30224d0c1
cgonzgo <cgonzgo@asesor.une.com.co>
2017-02-22 16:23:35 -0500

    Cambio Auditoria Grande

--- app/app.js
+++ app/app.js
@@ -13996,7 +13996,7 @@ app.controller('gestionAsignacionesCtrl', function ($scope, $rootScope, $locatio
             grupo: 'ASIGNACIONES',
             actividad: 'AUDITORIA'
         };
-        $scope.listarOpcionesAsginacion(opcionesAuditoria);
+        $scope.opcionesAuditoria=$scope.listarOpcionesAsginacion(opcionesAuditoria);
         services.buscarPedidoAuditoriafenix(pedido).then(
         	function (data) {
 				$scope.infoFenix	=	data.data[0];
