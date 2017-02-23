5aed13fba04987cfa7b8765a5a55f0920503935b
Sergio Alexis Zapata Mejia <szapame@epmtelco.com.co>
2017-02-23 13:55:58 -0500

    camb

--- app/app.js
+++ app/app.js
@@ -9427,7 +9427,6 @@ app.controller('AgendamientoCtrl', function ($scope, $rootScope, $location, $rou
 
 	$scope.set_color = function (service) {
          if (service.PROCESO == 'REPARACION' && service.FECHA_CITA_FENIX != '9999-00-00') {
-			//$scope.error = 'cccccccchhhhhhhiiiiiiccccchhhhaaarrroooon';
              $scope.error ="El pedido " + $scope.pedido1 + " estuvo agendado " + $scope.peds[0].FECHA_CITA_FENIX;
 			//alert('pedido especial');
 
