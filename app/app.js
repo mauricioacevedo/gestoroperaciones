2f31c171a33a67a668557019292129f18843895d
Sergio Alexis Zapata Mejia <szapame@epmtelco.com.co>
2017-03-08 12:19:00 -0500

    ca

--- app/app.js
+++ app/app.js
@@ -9287,7 +9287,8 @@ app.controller('AgendamientoCtrl', function ($scope, $rootScope, $location, $rou
 
         if ($scope.proceso == 'REPARACION' ||  $scope.pedido.TIEMPO_TOTAL >= 10 || $scope.pedido.TODAY_TRIES >= 3 || $scope.pedido.NOVEDAD != 'CIERRE POR ILOCALIZADO' || $scope.pedido.NOVEDAD != 'AGENDADO' ) {
             console.log($scope.pedido.TIEMPO_TOTAL)
-            console.log($scope.pedido.TIEMPO_TOTAL)
+            console.log($scope.pedido.TODAY_TRIES)
+            console.log($scope.pedido.NOVEDAD)
 
 
 			alert('debe colocar el pedido en la novedad CIERRE POR ILOCALIZADO o AGENDAR para continuar.');
