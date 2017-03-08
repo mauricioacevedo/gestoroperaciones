be2ad6aa94819996e7429b375d85027de4e6a0c5
Sergio Alexis Zapata Mejia <szapame@epmtelco.com.co>
2017-03-08 12:54:56 -0500

    ca

--- app/app.js
+++ app/app.js
@@ -9080,12 +9080,16 @@ app.controller('AgendamientoCtrl', function ($scope, $rootScope, $location, $rou
 
 
         if ($scope.proceso == 'REPARACION' && $scope.peds[0].TIEMPO_TOTAL>= 10 && $scope.peds[0].TODAY_TRIES>=3 ) {
-			alert("el PEDIDO tiene dias en el sistema" +$scope.peds[0].TIEMPO_TOTAL+ "y intentos" +$scope.peds[0].TODAY_TRIES+ "para continuar debe colocar la  novedad CIERRE POR ILOCALIZADO o AGENDAR.");
+			alert("el PEDIDO tiene dias en el sistema" + $scope.peds[0].TIEMPO_TOTAL + "0 intentos de contacto " + $scope.peds[0].TODAY_TRIES + "para continuar debe colocar la  novedad CIERRE POR ILOCALIZADO o AGENDAR.");
 			//return;
 		}
 
 		});
 
+        if ($scope.proceso == 'REPARACION' && $scope.peds[0].TIEMPO_TOTAL>= 10 && $scope.peds[0].TODAY_TRIES>=3 ) {
+			alert("el PEDIDO tiene dias en el sistema" + $scope.peds[0].TIEMPO_TOTAL + "0 intentos de contacto " + $scope.peds[0].TODAY_TRIES + "para continuar debe colocar la  novedad CIERRE POR ILOCALIZADO o AGENDAR.");
+			//return;
+		}
 
 
 		$scope.timeInit = new Date().getTime();
