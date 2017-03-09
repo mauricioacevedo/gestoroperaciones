52a749cc4734c2cafa936c159f96c454cdb00825
Sergio Alexis Zapata Mejia <szapame@epmtelco.com.co>
2017-03-09 17:40:01 -0500

    ca

--- app/app.js
+++ app/app.js
@@ -11094,6 +11094,7 @@ $scope.set_color_Cuartil = function (value) {
 		//$scope.ordenamientoDemepedidoNuevo=data.data['VALOR'];
 		$scope.ordenamientoDemepedidoUpdateactivacion = data.data['ULTIMA_ACTUALIZACION'];
 		$scope.UsuarioParametroactivacion = data.data['USUARIO_ID'];
+        console($scope.ordenamientoDemepedidoactivacion)
 		return data.data;
 	});
 
