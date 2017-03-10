13adf1a915bd920a536789c12a8b5500fe8d1892
Sergio Alexis Zapata Mejia <szapame@epmtelco.com.co>
2017-03-10 12:13:25 -0500

    ca

--- app/app.js
+++ app/app.js
@@ -11371,6 +11371,17 @@ app.controller('siebelActivacionCtrl', function ($scope, $rootScope, $location, 
 			return;
 		}
 
+          $scope.timeInit = new Date().getTime();
+		var date1 = new Date();
+		var year = date1.getFullYear();
+		var month = $scope.doubleDigit(date1.getMonth() + 1);
+		var day = $scope.doubleDigit(date1.getDate());
+		var hour = $scope.doubleDigit(date1.getHours());
+		var minute = $scope.doubleDigit(date1.getMinutes());
+		var seconds = $scope.doubleDigit(date1.getSeconds());
+
+        $scope.fecha_fin = year + "-" + month + "-" + day + " " + hour + ":" + minute + ":" + seconds;
+
 
 		services.insertTransaccionsiebelactivacion($scope.InfoGestion).then(
 
