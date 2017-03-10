b49bb1d5d3f4c8bd97ae5eb18a471211952c889b
Sergio Alexis Zapata Mejia <szapame@epmtelco.com.co>
2017-03-10 14:13:53 -0500

    cambio

--- app/app.js
+++ app/app.js
@@ -11356,8 +11356,6 @@ app.controller('siebelActivacionCtrl', function ($scope, $rootScope, $location, 
 			var df = new Date($scope.duracion);
 			$scope.duracion = $scope.doubleDigit(df.getHours() - 19) + ":" + $scope.doubleDigit(df.getMinutes()) + ":" + $scope.doubleDigit(df.getSeconds());
 
-        $scope.duracion = $scope.FECHA_FIN - $scope.FECHA_INICIO;
-        console.log($scope.duracion);
 
 		$scope.InfoGestion = {
 			//ID:gestion.ID,
