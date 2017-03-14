4b984bd8073fc9e6069f7e2c9135ab9312daceb2
Sergio Alexis Zapata Mejia <szapame@epmtelco.com.co>
2017-03-14 15:00:33 -0500

    ca

--- services/api.php
+++ services/api.php
@@ -6502,7 +6502,8 @@ class API extends REST {
         //counter
 
 
-        $query="SELECT count(*) as counter from gestor_seguimiento_activacion  where FECHA_ULTIMA_GESTOR between '$fechaini 00:00:00' and '$fechafin 23:59:59' ";
+        $query=" SELECT count(*) as counter "
+                " FROM gestor_historico_activacion ";
 
         $rr = $this->mysqli->query($query) or die($this->mysqli->error.__LINE__);
         $counter=0;
@@ -6534,6 +6535,19 @@ class API extends REST {
                 $counter2 = $row['counter'];
             }
         }
+
+        $query=" SELECT count(*) as counter ".
+            " FROM portalbd.gestor_activacion_tbl_pendi_gtc ";
+
+        $rr = $this->mysqli->query($query) or die($this->mysqli->error.__LINE__);
+        $counter3=0;
+        if($rr->num_rows > 0){
+            $result = array();
+            if($row = $rr->fetch_assoc()){
+                $counter3 = $row['counter'];
+            }
+        }
+
          $query=" SELECT count(*) as counter ".
             " FROM portalbd.gestor_activacion_pendientes_activador_dom ";
 
@@ -6570,7 +6584,7 @@ class API extends REST {
                 // var_dump($result);
             }
 
-            $this->response($this->json(array($result,$counter,$counter1,$counter2,$counter3,$counter4,$counter5)), 200); // send user details
+            $this->response($this->json(array($result,$counter,$counter1,$counter2,$counter3,$counter4)), 200); // send user details
         }
         $this->response('',204);        // If no records "No Content" status
 
