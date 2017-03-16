edbe9ea84a67e371aaeb7386a8310699c42cc0aa
Sergio Alexis Zapata Mejia <szapame@epmtelco.com.co>
2017-03-16 11:49:43 -0500

    ca

--- services/api.php
+++ services/api.php
@@ -8339,7 +8339,7 @@ class API extends REST {
        $parametroBusqueda= $this->buscarParametroFechaDemePedido('FECHA_ORDEN_DEMEPEDIDO_ACTIVACION');
 
 
-       if($TABLA!=""){
+        if($TABLA=='ACTIVADOR_SUSPECORE'){
 
            $TABLA = " from gestor_activacion_pendientes_activador_suspecore b " ;
 
