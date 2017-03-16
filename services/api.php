caf26046fc75fdafefd736549e85bde025feff68
Sergio Alexis Zapata Mejia <szapame@epmtelco.com.co>
2017-03-16 09:54:12 -0500

    ca

--- services/api.php
+++ services/api.php
@@ -13853,9 +13853,7 @@ class API extends REST {
         //si el actual usuario tenia un pedido "agarrado, hay que liberarlo"
         $pedido_actual = $this->_request['pedido_actual'];
 
-        if ($TABLA==""){
-
-        } else if($TABLA=='ACTIVADOR_SUSPECORE'){
+        if($TABLA=='ACTIVADOR_SUSPECORE'){
 
            $TABLA = " from gestor_activacion_pendientes_activador_suspecore b " ;
 
