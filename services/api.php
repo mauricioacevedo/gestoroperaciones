ffea0fdd13e083fe6d512561821b262f434498da
Sergio Alexis Zapata Mejia <szapame@epmtelco.com.co>
2017-03-22 16:39:53 -0500

    CA

--- services/api.php
+++ services/api.php
@@ -6575,7 +6575,7 @@ class API extends REST {
 
 
         $query= "SELECT ORDER_SEQ_ID,PEDIDO, ESTADO, FECHA_CREACION, FECHA_EXCEPCION ".
-                " , PRODUCTO,ASESOR,FECHA_GESTION,TIPIFICACION ".
+                " , PRODUCTO,ASESOR,FECHA_GESTION,TIPIFICACION,FECHA_INICIO,FECHA_FIN ".
                  " ,my_sec_to_time(timestampdiff(second,fecha_inicio,fecha_fin)) as DURACION ".
                 " from gestor_historico_activacion ".
                 " where FECHA_FIN between '$fechaini 00:00:00' and '$fechafin 23:59:59' ".
