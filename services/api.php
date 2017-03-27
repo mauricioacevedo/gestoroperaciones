12aa7a9acada03243aed372bc7941e016d435952
Sergio Alexis Zapata Mejia <szapame@epmtelco.com.co>
2017-03-27 15:49:53 -0500

    cambio insert

--- services/api.php
+++ services/api.php
@@ -9840,11 +9840,13 @@ class API extends REST {
 
             //----------insert
 
+            if( $tabla=='ACTIVADOR_SUSPECORE'){
             if( $TIPIFICACION=='FINALIZADA'){
             $sqlupdate="update gestor_activacion_pendientes_activador_suspecore set FECHA_CARGA = '$today',STATUS='CERRADO_ACTI',FECHA_EXCEPCION = '$FECHA_EXCEPCION' WHERE ID=$ID";
            //  echo $sqlupdate;
 
        }
+    }
              // echo $sqlupdate;
             $rr = $this->mysqli->query($sqlupdate) or die($this->mysqli->error.__LINE__);
 
