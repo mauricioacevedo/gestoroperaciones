2020935b0fb4507310194807e48cf842bbcd46ee
Sergio Alexis Zapata Mejia <szapame@epmtelco.com.co>
2017-03-28 12:32:56 -0500

    FFF

--- app/app.js
+++ app/app.js
@@ -11314,7 +11314,7 @@ app.controller('siebelActivacionCtrl', function ($scope, $rootScope, $location, 
 					grupo: $scope.peds[0].GRUPO,
 					actividad: $scope.peds[0].ACTIVIDAD
                 };
-                //console.log(opciones);
+                console.log(data.data);
 
                $scope.listarOpcionesAsginacion(opciones);
 
