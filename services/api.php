f39a32a2074e1ce7a67ec18e416d0d56740dbef5
cgonzgo <cgonzgo@asesor.une.com.co>
2017-04-07 06:38:57 -0500

    Cambios en el Login

--- services/api.php
+++ services/api.php
@@ -1,6 +1,6 @@
 <?php
-//error_reporting(E_ALL);
-//ini_set('display_errors', '1');
+error_reporting(E_ALL);
+ini_set('display_errors', '1');
 
 require_once("Rest.inc.php");
 //include_once("/var/www/html/gestorasignaciones/conn_fenix.php");
