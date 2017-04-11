92d06ebe84efbd45c3b35ec173b9747a1886dc26
cgonzgo <cgonzgo@asesor.une.com.co>
2017-04-11 08:09:48 -0500

    CRUD Tareas/Requerimientos

--- services/api.php
+++ services/api.php
@@ -16303,7 +16303,7 @@ class API extends REST {
         $sql =  " SELECT t.*, tp.PIC FROM portalbd.go_task t ".
                 " left join portalbd.go_task_profile tp on t.USUARIO_GEST=tp.USUARIO_ID ".
                 " where 1=1 ".
-                " and ESTADO='ACTIVO' ";
+                " order by t.IDTAREA desc ";
 
         // echo $sql;
         $r = $this->mysqli->query($sql);
