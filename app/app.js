e8f4cba114cca88e2b928ffcd1e89c70aadf6624
Sergio Alexis Zapata Mejia <szapame@epmtelco.com.co>
2017-04-18 09:05:50 -0500

    ca

--- app/app.js
+++ app/app.js
@@ -11277,13 +11277,6 @@ app.controller('siebelActivacionCtrl', function ($scope, $rootScope, $location, 
 
    
 
-        services.getListadoActivacion($scope.data.fechaIni, $scope.data.fechaFin, $scope.data.currentPage).then(function (data) {
-            $scope.listadoactivacion = data.data[0];
-            return data.data;
-        });
-
-
-    };
 
 
 	//  ---------------------------------fin logueo-------------------------------------------
