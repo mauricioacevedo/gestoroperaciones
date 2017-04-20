0049da155f61e25361684f89aa79de54b1b0e4c4
cgonzgo <cgonzgo@asesor.une.com.co>
2017-04-20 09:27:56 -0500

    Arreglos en querys del API

--- app/app.js
+++ app/app.js
@@ -14992,7 +14992,7 @@ $scope.getTaskCrud = function () {
     };
 
 $scope.updateUsers = function (username) {
-        $scope.task.crud.USUARIO_GEST = $filter('filter')($scope.task.crud, username);
+        $scope.task.crud = $filter('filter')($scope.task.crud, username);
     };
 
 $scope.getTaskCrud();
