19cec9ba14789d0de972be73d1343c94e229ea0f
cgonzgo <cgonzgo@asesor.une.com.co>
2017-04-20 13:34:53 -0500

    Nuevo Cuadro Indicadores - Alarmados Proactivos

--- app/app.js
+++ app/app.js
@@ -3642,7 +3642,7 @@ app.controller('IndicadoresCtrl', function ($scope, $rootScope, $location, $rout
         var output = d.getFullYear() + '-' + (month<10 ? '0' : '') + month + '-' + (day<10 ? '0' : '') + day;
 		var ahora = $rootScope.fechaProceso();
 		var horaLimite = output + ' 18:00:00';
-        var diferencia = Math.abs(Date.parse(horaLimite) - Date.parse(ahora)) / 36e5;
+        var diferencia = -(Math.abs(Date.parse(horaLimite) - Date.parse(ahora)) / 36e6);
 
 		console.log('ahora: '+ ahora);
         console.log('Limite: '+ horaLimite);
