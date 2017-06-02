a146fbf42d518399e2a37a8a044e90652b283127
Sergio Alexis Zapata Mejia <szapame@epmtelco.com.co>
2017-06-02 12:50:36 -0500

    camp

--- app/app.js
+++ app/app.js
@@ -8844,7 +8844,7 @@ app.controller('AgendamientoCtrl', function ($scope, $rootScope, $location, $rou
 			if ($scope.pedidos == "") {
 				$scope.pedidos = new Array();
 			}
-			$scope.pedidos = $scope.pedidos.concat($scope.pedido);
+			$scope.pedidos = $scope.pedidos.concat(angular.copy($scope.pedido));
 			if ($scope.historico_pedido == "") {
 				$scope.historico_pedido = new Array();
 			}
