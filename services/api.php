5b81e07c496fdd2a892593382f74a61ca96a2bb2
cgonzgo <cgonzgo@asesor.une.com.co>
2017-06-05 13:47:39 -0500

    Funcion para buscar usuarios en LDAP

--- services/api.php
+++ services/api.php
@@ -17904,9 +17904,11 @@ $query="SELECT count(*) as counter from gestor_pendientes_reagendamiento a where
         $userBusqueda   = strtoupper($userBusqueda);
 
         $ldapserver     =   'net-dc05';
-        $user           =   "CGONZGO";
+        //$user           =   "CGONZGO";
+        $user           = "sape";
         $ldapuser       =   "EPMTELCO\\$user";
-        $ldappass       =   addslashes("Mangoperajunio2017*");
+        //$ldappass       =   addslashes("Mangoperajunio2017*");
+        $ldappass       =   addslashes("n0sun32008*");
         $ldaptree       =   "OU=Usuarios,DC=epmtelco,DC=com,DC=co";
         $varuser        =   "(samaccountname=$userBusqueda)";
 
