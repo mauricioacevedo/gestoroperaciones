46d70900e3fa8659d5d356730964dc4301ab3fbd
cgonzgo <cgonzgo@asesor.une.com.co>
2017-06-06 16:29:47 -0500

    Funcion para buscar usuarios en LDAP

--- services/api.php
+++ services/api.php
@@ -17958,9 +17958,7 @@ $query="SELECT count(*) as counter from gestor_pendientes_reagendamiento a where
                             $nombre = $row['NOMBRE'];
 
                         }
-                        if($object->PICTURE=''){
-                            $object->PICTURE = null;
-                        }
+
 
                         $this->response($this->json(array($object, $userBusqueda)), 200);
 
