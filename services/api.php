0603a71827d63a417e4e7d673339d2ddcfb331fa
Sergio Alexis Zapata Mejia <szapame@epmtelco.com.co>
2017-06-09 10:16:29 -0500

    cambio insert

--- services/api.php
+++ services/api.php
@@ -10392,7 +10392,7 @@ $query="SELECT count(*) as counter from gestor_pendientes_reagendamiento a where
             if($tabla=='AMARILLAS' ){
                 if($TIPIFICACION=='FINALIZADA' || $TIPIFICACION=='RENUMERAR'){
                     $sqlupdate="update pendientes_amarillas set FECHA_CARGA = '$today',STATUS='CERRADO_ACTI',FECHA_EXCEPCION = '$FECHA_EXCEPCION' WHERE PEDIDO='$PEDIDO' and STATUS='PENDI_ACTI'";
-                      echo $sqlupdate;
+                      //echo $sqlupdate;
                 }else {
 
                     $sqlupdate="update pendientes_amarillas set FECHA_CARGA = '$today',STATUS='MALO',FECHA_EXCEPCION = '$FECHA_EXCEPCION' WHERE ID=$ID and STATUS='PENDI_ACTI'";
