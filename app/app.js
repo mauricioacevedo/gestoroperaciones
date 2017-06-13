c172a46a3c5ab2bd4cd1a6c5f07263b9ee3da349
Sergio Alexis Zapata Mejia <szapame@epmtelco.com.co>
2017-06-13 09:10:53 -0500

    cambio fecha inicio

--- app/app.js
+++ app/app.js
@@ -11832,7 +11832,7 @@ app.controller('siebelActivacionCtrl', function ($scope, $rootScope, $location, 
 		$scope.InfoPedido = {};
 		//$scope.InfoGestion = [];
 		$scope.FECHA_CREACION = null;
-        $scope.fecha_inicio = null;
+        //$scope.fecha_inicio = null;
 		$scope.accRdy = false;
 		$scope.InfoGestion = {};
 		$scope.pedidoIsGuardado = false;
