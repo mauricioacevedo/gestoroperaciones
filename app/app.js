51a30539b68986e6356d653f1692af640bd82bd7
cgonzgo <cgonzgo@asesor.une.com.co>
2017-06-28 10:37:12 -0500

    Pruebas con NodeJS

--- app/app.js
+++ app/app.js
@@ -1475,7 +1475,7 @@ app.controller('pushNotificationsCtrl', function ($scope, $rootScope, $location,
     $scope.template = '';
 
     var tituloPagina = $window.document.title;
-    console.log($location);
+    console.log($route.current.originalPath);
 
     $scope.play = function() {
         $scope.audio.play();
