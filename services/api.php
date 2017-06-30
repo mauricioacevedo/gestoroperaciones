e4ab574826a848ef27a3cfab4b7f00c5e1e856f8
Sergio Alexis Zapata Mejia <szapame@epmtelco.com.co>
2017-06-30 12:13:31 -0500

    cambio guardar importar

--- services/api.php
+++ services/api.php
@@ -11935,12 +11935,12 @@ private function cargar_datos_activacion(){
         list($end, $end_h, $end_v) = $rslt;
 
         //empieza  lectura vertical
-       // $table = "<table  border='1'>";
+        $table = "<table  border='1'>";
         for($v=$start_v; $v<=$end_v; $v++){
             //empieza lectura horizontal
 
             if ($v==1) continue;
-           // $table .= "<tr>";
+            $table .= "<tr>";
             $filas= $start_h + 1;
 
 
@@ -11952,24 +11952,24 @@ private function cargar_datos_activacion(){
                  if($h=="A"){
                     $timestamp = PHPExcel_Shared_Date::ExcelToPHP($cellValue1);//fecha larga
                     $cellValue1 = gmdate("Y-m-d H:i:s",$timestamp);//fecha formateada+
-                  //  $table .= "<td>";
+                    $table .= "<td>";
                 }
                  if($h=="B"){
                     $timestamp = PHPExcel_Shared_Date::ExcelToPHP($cellValue1);//fecha larga
                     $cellValue1 = gmdate("H:i:s",$timestamp);//fecha formateada+
-                 //   $table .= "<td>";
+                    $table .= "<td>";
                 }
                 if($h=="M"){
                     $timestamp = PHPExcel_Shared_Date::ExcelToPHP($cellValue1);//fecha larga
                     $cellValue1 = gmdate("Y-m-d H:i:s",$timestamp);//fecha formateada+
-               //     $table .= "<td>";
+                    $table .= "<td>";
                 }
                 
 
               //  $table .= "<td>";
                 if($cellValue1 !== null){
-              //     $table .= $cellValue1;
-             //    $table .= "</td>";
+              /    $table .= $cellValue1;
+             /   $table .= "</td>";
                 // echo  $cellValue1;   
                 }
                 
@@ -11987,7 +11987,7 @@ private function cargar_datos_activacion(){
             
                 $sqldatos="insert into gestor_historico_activacion (FECHA_EXCEPCION,HORA,PEDIDO,PRODUCTO,TRANSACCION,APLICATIVO,OBSERVACION,NUMERO_CR,TIPIFICACION,ASESOR
 ,PEDIDO_FENIX,TABLA,FECHA_GESTION,SOURCE,FECHA_INICIO,FECHA_FIN) values ($guardar,'MANUAL',$today,$today)";
-               echo  $sqldatos;
+            //   echo  $sqldatos;
                 $r = $this->mysqli->query($sqldatos) or die($this->mysqli->error.__LINE__);
 
                
@@ -12016,7 +12016,7 @@ private function cargar_datos_activacion(){
 
 
 
-        //   $table .= "</tr>";
+           $table .= "</tr>";
         }
 
        
