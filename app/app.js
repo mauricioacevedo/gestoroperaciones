1419071ecab0ec7b9189cc5953c630ffb211f220
cgonzgo <cgonzgo@asesor.une.com.co>
2017-07-04 08:55:09 -0500

    Correccion de Errores: - Window Open no funciona correctamente

--- app/app.js
+++ app/app.js
@@ -17286,8 +17286,8 @@ app.run(function ($rootScope, $compile, $window, notify, services) {
 	$rootScope.BuscaNodo = function () {
 		var urlNodoHfc = "http://10.100.82.125/gestoroperaciones/#/cmts/";
         var params = 'scrollbars=no,resizable=no,status=no,location=no,toolbar=no,menubar=no,width=600,height=300,left=100,top=100';
-        $rootScope.bNodoHfc = $window.open(urlNodoHfc, 'Buscar Nodo HFC','target _blank', params);
-        console.log($rootScope.bNodoHfc);
+        $rootScope.btnUrls.bNodoHfc = $window.open(urlNodoHfc, 'Buscar Nodo HFC','target _blank', params);
+        console.log($rootScope.btnUrls.bNodoHfc);
         //var bNodoWin = $window.open(window.location.pathname + "#/cmts/", "toolbar=yes, scrollbars=yes, resizable=yes, top=50, left=500, width=700, height=600");
 	};
 	$rootScope.BuscaGPON = function () {
