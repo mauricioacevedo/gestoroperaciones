352cc19f4126596633e542308e75125c5403d4a5
Sergio Alexis Zapata Mejia <szapame@epmtelco.com.co>
2017-07-05 14:49:10 -0500

    LISTADO AMARILLAS

--- services/api.php
+++ services/api.php
@@ -7004,7 +7004,7 @@ $query="SELECT count(*) as counter from gestor_pendientes_reagendamiento a where
             }
         }
         $query= "SELECT ORDER_SEQ_ID,PEDIDO, ESTADO, FECHA_CREACION, FECHA_EXCEPCION,TRANSACCION ".
-            " , PRODUCTO,ASESOR,FECHA_GESTION,TIPIFICACION,FECHA_INICIO,FECHA_FIN ".
+            " , PRODUCTO,ASESOR,FECHA_GESTION,TIPIFICACION,FECHA_INICIO,FECHA_FIN,TABLA ".
             " ,my_sec_to_time(timestampdiff(second,fecha_inicio,fecha_fin)) as DURACION ".
             " from gestor_historico_activacion ".
             " where fecha_fin between '$fechaini 00:00:00' ".
