952c9f7a39738ce0ca91bdbcc80b0abe706ee443
Sergio Alexis Zapata Mejia <szapame@epmtelco.com.co>
2017-07-05 15:21:58 -0500

    error echo amarillas

--- services/api.php
+++ services/api.php
@@ -12003,13 +12003,13 @@ private function cargar_datos_activacion(){
                 if($TIPIFICACION=='CR'){
 
                     $sqlupdate="update pendientes_amarillas set FECHA_CARGA = '$today',STATUS='MALO',FECHA_EXCEPCION = '$FECHA_EXCEPCION' WHERE PEDIDO='$PEDIDO' and STATUS='PENDI_ACTI'";
-                      //echo $sqlupdate;
+                      echo $sqlupdate;
                 }else {
 
                     $sqlupdate="update pendientes_amarillas set FECHA_CARGA = '$today',STATUS='CERRADO_ACTI',FECHA_EXCEPCION = '$FECHA_EXCEPCION' WHERE PEDIDO='$PEDIDO' and STATUS='PENDI_ACTI'";
                 }
             }
-             echo $sqlupdate;
+             //echo $sqlupdate;
             $rr = $this->mysqli->query($sqlupdate) or die($this->mysqli->error.__LINE__);
 
 
