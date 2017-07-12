4ed97d9b0a46f6f1243374e59ee55e8f8a20e835
Sergio Alexis Zapata Mejia <szapame@epmtelco.com.co>
2017-07-12 15:39:37 -0500

    cambio importa agend

--- services/api.php
+++ services/api.php
@@ -11664,7 +11664,7 @@ $query="SELECT count(*) as counter from gestor_pendientes_reagendamiento a where
                 $r = $this->mysqli->query($sqlbodega_datos) or die($this->mysqli->error.__LINE__);
 
                 $sqldatos="insert into portalbd.gestor_historicos_reagendamiento (PEDIDO_ID,CLIENTE_ID,ACCESO,FUENTE,FECHA_FIN,ASESOR,NOVEDAD,OBSERVACION_GESTOR) values ('$PEDIDO_ID','$cliente_id','$ACCESO','$FUENTE','$FECHA_FIN','$usas','$NOVEDAD','$OBSERVACION_GESTOR')";
-                //echo  $sqldatos;
+                echo  $sqldatos;
                 $r = $this->mysqli->query($sqldatos) or die($this->mysqli->error.__LINE__);
 
 
