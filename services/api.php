617b8229dd11c67218c13fcb8a962b1ba6ddd9a1
Sergio Alexis Zapata Mejia <szapame@epmtelco.com.co>
2017-07-13 13:58:25 -0500

    cambio guardar importar

--- services/api.php
+++ services/api.php
@@ -11666,7 +11666,7 @@ $query="SELECT count(*) as counter from gestor_pendientes_reagendamiento a where
 
                 $table .= "<td>";
                 $guardar .=" '$cellValue',";
-
+                 echo $guardar;           
                 if($cellValue!== null){
                     $table .= $cellValue;
                 }
