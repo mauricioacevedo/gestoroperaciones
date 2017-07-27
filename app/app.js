191506d542d2a3b5813ba7c59793616c8948230e
cgonzgo <cgonzgo@asesor.une.com.co>
2017-07-27 13:19:08 -0500

    TransaccionesNCA - La forma manual ahora guarda en pedidos. - Se agrega listado de Municipios. - Se agrega Municipio al demePedido con conceptos de Siebel

--- app/app.js
+++ app/app.js
@@ -15867,6 +15867,7 @@ app.controller('gestionAsignacionesCtrl', function ($scope, $rootScope, $locatio
             function (data) {
                 $scope.listadoMunicipios=data.data;
                 console.log($scope.municipio);
+                $scope.iplaza = {MUNICIPIO_ID: $scope.municipio};
                 return data.data;
 
             },
