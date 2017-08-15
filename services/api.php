a9271b5e765f782fdfda29460dd0c678b5a6e4fb
Sergio Alexis Zapata Mejia <szapame@epmtelco.com.co>
2017-08-15 10:49:46 -0500

    cambio update demepedido act

--- services/api.php
+++ services/api.php
@@ -9052,11 +9052,11 @@ class API extends REST {
                 $sep=",";
             }
             
-   if($tabla=='ACTIVADOR_SUSPECORE'){
+   if($tabla!='ACTIVADOR_SUSPECORE'){
 
-            $sqlupdate="update gestor_activacion_pendientes_activador_suspecore set ASESOR='$user' where ID in ($ids) and pedido='$mypedido'";
-        }else {
             $sqlupdate="update gestor_activacion_pendientes_activador_dom set ASESOR='$user' where ID in ($ids) and pedido='$mypedido'";
+        }else {
+            $sqlupdate="update gestor_activacion_pendientes_activador_suspecore set ASESOR='$user' where ID in ($ids) and pedido='$mypedido'";
         }
 
              echo $sqlupdate;          
