fc08b3c3e3c6a51051dc2eadba29112217b6ea4d
Sergio Alexis Zapata Mejia <szapame@epmtelco.com.co>
2017-09-06 11:28:02 -0500

    cambio activacion

--- app/app.js
+++ app/app.js
@@ -11415,12 +11415,12 @@ app.controller('ActivacionCtrl',function ($scope, $rootScope, $location, $routeP
                   //  console.log(data);
                        $scope.listadoactivacion=data.data[0];
                        $scope.totalItems=data.data[1];
-                        $scope.data.totalItems2=data.data[2];
-                        $scope.data.totalItems3=data.data[3];
-                        $scope.data.totalItems4=data.data[4];
-                        $scope.data.totalItems5=data.data[5];
-                        $scope.data.totalItems6=data.data[6];
-						$scope.data.totalItems7=data.data[7];
+                       // $scope.data.totalItems2=data.data[2];
+                        //$scope.data.totalItems3=data.data[3];
+                        //$scope.data.totalItems4=data.data[4];
+                        //$scope.data.totalItems5=data.data[5];
+                        //$scope.data.totalItems6=data.data[6];
+						//$scope.data.totalItems7=data.data[7];
 
                     console.log(data.data[1]);
                    // console.log($scope.totalItems);
