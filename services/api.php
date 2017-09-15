1ed893b1fd2287d273ef4b551b168d901ee83e4a
Sergio Alexis Zapata Mejia <szapame@epmtelco.com.co>
2017-09-15 16:27:30 -0500

    cambio

--- services/api.php
+++ services/api.php
@@ -8500,8 +8500,8 @@ class API extends REST {
 
                 if($plaza=="TODOS"){//para que sea posible obtener un registro de cualquier plaza
                     
-                    // $plaza2="AND MUNICIPIO_ID IN (select a.MUNICIPIO_ID from tbl_plazas a where a.PLAZA not in ('BOGOTA'))";
-                    $plaza2="AND MUNICIPIO_ID NOT IN ('BOGCUNCOL','BOG','BOGOTA','BOG-COBRE','BOGBOGCOL','+BOGOTA')";
+                     $plaza2="AND MUNICIPIO_ID  NOT IN (select a.MUNICIPIO_ID from tbl_plazas a where a.PLAZA not in ('BOGOTA','BOG-COBRE'))";
+
 
                 }else{
                     $plaza2=" AND MUNICIPIO_ID IN (select a.MUNICIPIO_ID from tbl_plazas a where a.PLAZA='$plaza') ";
