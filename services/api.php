738ee852a622b3327cd4f252f430097a46a38534
JHOR MAYKOL BEDOYA GOMEZ <jbedogom@epmtelco.com.co>
2017-09-22 11:28:31 -0500

    CAMBIO PLAZA TODOS AGAIN

--- services/api.php
+++ services/api.php
@@ -8497,7 +8497,7 @@ class API extends REST {
 
                 if($plaza=="TODOS"){//para que sea posible obtener un registro de cualquier plaza
                     
-                    $plaza2=" AND NOMBRE_DEPARTAMENTO IN (select a.MUNICIPIO_ID from tbl_plazas a where a.NOMBRE_DEPARTAMENTO = 'ANTIOQUIA')";
+                    $plaza2=" AND NOMBRE_DEPARTAMENTO IN (select a.NOMBRE_DEPARTAMENTO from tbl_plazas a where a.NOMBRE_DEPARTAMENTO = 'ANTIOQUIA')";
                     //Obtener un registro de cualquier plaza menos los de bogota
                     
                 }else{
