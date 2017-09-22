191cbd47e1e5abe7c7ab448ae2a5ef99cbcd92d8
JHOR MAYKOL BEDOYA GOMEZ <jbedogom@epmtelco.com.co>
2017-09-22 15:53:00 -0500

    final change all place

--- services/api.php
+++ services/api.php
@@ -8497,7 +8497,7 @@ class API extends REST {
 
                 if($plaza=="TODOS"){//para que sea posible obtener un registro de cualquier plaza
                     
-                    $plaza2=" AND MUNICIPIO_ID IN (select a.MUNICIPIO_ID from tbl_plazas a where a.MUNICIPIO_ID NOT IN ('BOG-COBRE'))";
+                $plaza2=" AND MUNICIPIO_ID IN (select a.MUNICIPIO_ID from tbl_plazas a where a.MUNICIPIO_ID NOT IN ('BOG-COBRE','BOGCUNCOL'))";
                     //Obtener un registro de cualquier plaza menos los de bogota
                     
                 }else{
@@ -8667,7 +8667,7 @@ class API extends REST {
         }
 
         if($plaza=="TODOS"){//para que sea posible obtener un registro de cualquier plaza
-            $plaza="AND MUNICIPIO_ID IN (select a.MUNICIPIO_ID from tbl_plazas a where a.MUNICIPIO_ID NOT IN ('BOG-COBRE'))";
+            $plaza="AND MUNICIPIO_ID IN (select a.MUNICIPIO_ID from tbl_plazas a where a.MUNICIPIO_ID NOT IN ('BOG-COBRE','BOGCUNCOL'))";
         }else{
             //$plaza=" AND b.MUNICIPIO_ID IN (select a.MUNICIPIO_ID from tbl_plazas a where a.PLAZA='$plaza') ";
             $plaza=" AND b.MUNICIPIO_ID IN ('$plaza') ";
