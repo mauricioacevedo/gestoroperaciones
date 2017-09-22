36aa40a0ac8544e1a8fffa9b5274ec97730e8cb0
JHOR MAYKOL BEDOYA GOMEZ <jbedogom@epmtelco.com.co>
2017-09-22 12:44:05 -0500

    change plaza all again

--- services/api.php
+++ services/api.php
@@ -8536,7 +8536,7 @@ class API extends REST {
                     $plaza2.
                     " ORDER BY FECHA_ESTADO ASC ";
 
-                echo $sqlllamadas;
+
 
                 $rr = $this->mysqli->query($sqlllamadas) or die($this->mysqli->error.__LINE__);
 
