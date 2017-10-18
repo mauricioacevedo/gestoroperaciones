ff75819a0ec5e0852f23f7be133daf020852f69a
JHOR MAYKOL BEDOYA GOMEZ <jbedogom@epmtelco.com.co>
2017-10-18 16:22:45 -0500

    Again

--- app/app.js
+++ app/app.js
@@ -17409,7 +17409,7 @@ app.config(['$routeProvider',
             cargos: ['1','2','3','4','5','6','7','8','9']
 		})
 
-      .when('/kpis/transaccion', {
+      .when('/kpis/', {
 			title: 'NCA',
 			templateUrl: 'partials/transaccion-kpis.html',
 			controller: 'NCACtrl',
