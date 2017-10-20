2c8436c187eb9785ae368fbe78196d72f1a09b99
JHOR MAYKOL BEDOYA GOMEZ <jbedogom@epmtelco.com.co>
2017-10-20 14:16:23 -0500

    www

--- services/api.php
+++ services/api.php
@@ -9038,7 +9038,7 @@ private function demePedidoEdatel(){
 
             }
 
-
+/*
         $query1="select b.PEDIDO_ID,b.SUBPEDIDO_ID,b.SOLICITUD_ID,b.FECHA_ESTADO,b.FECHA_INGRESO,b.FECHA_CITA ".
             ",(SELECT a.user FROM vistas_pedidos  a where a.user='$user' AND b.PEDIDO_ID=a.pedido_id ".
             " AND a.fecha BETWEEN  '$today 00:00:00' AND  '$today 23:59:59' limit 1) as BEENHERE ".
@@ -9049,7 +9049,7 @@ private function demePedidoEdatel(){
             $plaza.
             //" and b.CONCEPTO_ID='$concepto' ".
             //" AND b.MUNICIPIO_ID IN (select a.MUNICIPIO_ID from tbl_plazas a where a.PLAZA='$plaza') ".
-            " order by b.$parametroBusqueda ASC";
+            " order by b.$parametroBusqueda ASC";*/
 
 
        // echo $query1;
