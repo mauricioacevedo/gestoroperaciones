39cbdd12bc5c4db02703c931adba3f6c295516c5
JHOR MAYKOL BEDOYA GOMEZ <jbedogom@epmtelco.com.co>
2017-10-24 15:38:28 -0500

    Debug InsertPedidoEdatel

--- services/api.php
+++ services/api.php
@@ -2518,14 +2518,14 @@ class API extends REST {
 
         $today = date("Y-m-d H:i:s");
         $query = "INSERT INTO pedidos (pedido, fuente, actividad, estado,".
-            " fecha_estado, concepto_anterior, concepto_final, source, pedido_id ,municipio_id ".
+            " fecha_estado, concepto_anterior, concepto_final, source, pedido_id ,municipio_id, ".
             " DEPARTAMENTO, fuente, ACCION, FECHA_INICIO ,user , duracion) ".
             " values ( ".
             " '$SOLICITUD','$FUENTE','$ACTIVIDAD','$ESTADO','$FECHA_CARGA','$TIPO_TRANSACCION','$TIPO_TRANSACCION','$SOURCE','$SOLICITUD','$COD_LOCALIDAD','$LOCALIDAD', ".
             " '$GEOREFERENCIA','$REDSUGERIDA','$FECHA_CARGA','$useri','$Duracion') ";
 
 
-        echo var_dump($query);
+        //echo var_dump($query);
 
         $rr = $this->mysqli->query($query) or die($this->mysqli->error.__LINE__);
 
