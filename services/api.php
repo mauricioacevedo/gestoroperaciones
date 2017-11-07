ecb1162892e45f7ad9939d11172216173e3ceb73
JHOR MAYKOL BEDOYA GOMEZ <jbedogom@epmtelco.com.co>
2017-11-07 07:43:35 -0500

    mm

--- services/api.php
+++ services/api.php
@@ -8684,7 +8684,7 @@ class API extends REST {
                     break;
                 }
             }
-
+            echo var_dump($concepto);
 
             $concepto=" and b.CONCEPTO_ID in ('$concepto')";
         }else if($concepto=="STBOX"){
@@ -8694,6 +8694,7 @@ class API extends REST {
         }else if($concepto=="EQURED"){
 
             $concepto=" and b.CONCEPTO_ID in ('PETEC') and (b.RADICADO_TEMPORAL = 'EQURED' )";
+
 
         }else if($concepto=="14" || $concepto=="99" || $concepto=="O-101" || $concepto=="OT-C08" ){
             echo var_dump("INGRESO");
