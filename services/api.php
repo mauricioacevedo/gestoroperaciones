3d4e78074ebafc863e0a6497d8d8d02a5d1dae26
JHOR MAYKOL BEDOYA GOMEZ <jhor.Bedoya@asesor.une.com.co>
2017-11-27 09:20:02 -0500

    www

--- services/api.php
+++ services/api.php
@@ -8480,7 +8480,7 @@ class API extends REST {
         $username       =   $this->_request['username'];
         $prioridad      =   $this->_request['prioridad'];
 
-        echo var_dump($plaza);
+        echo var_dump($this);
 
         $filename = '../tmp/control-threads.txt';
         if(file_exists($filename)){
