2564f1dd3746d7ee08906912cef82f6acb014bbb
JHOR MAYKOL BEDOYA GOMEZ <jhor.Bedoya@asesor.une.com.co>
2017-11-27 09:52:46 -0500

    WWW

--- app/app.js
+++ app/app.js
@@ -14862,7 +14862,7 @@ app.controller('AsignacionesCtrl', function ($scope, $rootScope, $location, $rou
             $scope.iplaza.MUNICIPIO_ID ="TODOS";
         }
 
-        if ($scope.iplaza == "BOGOTA"){
+        if ($scope.iplaza == "BOGCUNCOL"){
             $scope.iplaza={};
             $scope.iplaza.MUNICIPIO_ID ="BOGCUNCOL";
         }
