6220728fd344cb99c87cdd4bb84db7be6c7209bd
Mauricio Acevedo <mauricio.acevedo@une.com.co>
2017-12-05 10:34:51 -0500

    www

--- services/api.php
+++ services/api.php
@@ -8589,7 +8589,7 @@ class API extends REST {
                 if ($plaza == "BOGOTA" || $plaza == "BOGCUNCOL")
                 {
 
-                    $plaza2=" AND MUNICIPIO_ID IN (select a.MUNICIPIO_ID from tbl_plazas a where a.MUNICIPIO_ID IN ('BOG-COBRE','BOGCUNCOL')) ";
+                    $plaza=" AND MUNICIPIO_ID IN (select a.MUNICIPIO_ID from tbl_plazas a where a.MUNICIPIO_ID IN ('BOG-COBRE','BOGCUNCOL','BOGOTA')) ";
                     //$plaza2=" AND MUNICIPIO_ID IN (select a.MUNICIPIO_ID from tbl_plazas a where a.PLAZA='$plaza') ";
                     //$plaza2=" AND b.MUNICIPIO_ID IN ('$plaza') ";
                 }
