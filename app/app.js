bda6a37654867ac1ef232e84b470591ee6698df7
Mauricio Acevedo <mauricio.acevedo@une.com.co>
2017-12-07 08:47:30 -0500

    importe pni

--- app/app.js
+++ app/app.js
@@ -79,7 +79,7 @@ app.service('fileUploadPNI', ['$http', '$cookieStore', function ($http, $cookieS
 		var user = $cookieStore.get('logedUser').login;
 		file['user'] = user + '6666666';
 		fd.append('user', user);
-		fd.append('fileUpload', file);
+		fd.append('fileUploadPNI', file);
 		//$http.post('services/cargar_datos', fd, {
         $http.post(uploadUrl, fd, {
 				withCredentials: false,
