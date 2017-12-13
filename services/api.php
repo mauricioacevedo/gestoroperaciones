3e08b8ed1630cf91c69da7340e29f71ea02ebcf3
Mauricio Acevedo <mauricio.acevedo@une.com.co>
2017-12-13 10:16:14 -0500

    forma kpis

--- services/api.php
+++ services/api.php
@@ -10934,7 +10934,7 @@ private function demePedidoEdatel(){
         $transaccion = $transaccion['gestion'];
         $fechaini = $fecha['fechainicio'];
 
-        //echo var_dump ($fechaini);
+        echo var_dump ($fecha);
 
         $column_names = array('NEGOCIO','FECHASOLICI','ITEMS','SISTEMAINFO','RESULTADOCARGA','ITEMSPROCESADO','ITEMSINCONSISTENTES','OBSERVACIONES');
 
