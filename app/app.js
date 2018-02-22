3f23b07175d8614b356d58b4fdbf68dea6dfa8e0
Juan Jose Martinez Jimenez <Juan.J.Martinez@asesor.une.com.co>
2018-02-22 15:43:43 -0500

    CR

--- app/app.js
+++ app/app.js
@@ -798,7 +798,9 @@ app.factory("services", ['$http', '$timeout', function ($http) {
     };
 
     obj.ActualizarRegistroCR = function(gestion,fechainicio){
-        return $http.post(serviceBase + 'actualizarTransaccionCR'),
+        return $http.post(serviceBase + 'actualizarTransaccionCR',{
+
+        });
 
 
 
