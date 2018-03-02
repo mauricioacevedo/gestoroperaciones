8b7c469ccc36d382a465707711740c3a3c2c2538
Juan Jose Martinez Jimenez <Juan.J.Martinez@asesor.une.com.co>
2018-03-02 10:24:57 -0500

    CR

--- app/app.js
+++ app/app.js
@@ -831,8 +831,9 @@ app.factory("services", ['$http', '$timeout', function ($http) {
     };
 
 
-    obj.editTransaccionActividadescr2 = function (INCIDENTE) { //listado alarmas activacion
+    obj.editTransaccionActividadescr2 = function (INCIDENTE,FECHA_CIERRE) { //listado alarmas activacion
 		return $http.get(serviceBase + 'editTransaccionActividadescr2?INCIDENTE=' + INCIDENTE);
+        return $http.get(serviceBase + 'editTransaccionActividadescr2?INCIDENTE=' + FEHA_CIERRE);
 	};
 
 
