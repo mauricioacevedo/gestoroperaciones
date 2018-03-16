4b29fc27c797e3025f9667fdd0cc5ba8ec490830
Mauricio Acevedo <mauricio.acevedo@tigoune.com.co>
2018-03-16 01:21:57 -0500

    Fix de insert de ingresos de pedidos manuales

--- services/api.php
+++ services/api.php
@@ -1494,10 +1494,10 @@ class API extends REST {
             //echo $query;
             $r = $this->mysqli->query($query) or die($this->mysqli->error.__LINE__);
 
-            $sqlInsertIngresos="insert into informe_petec_pendientesm (PEDIDO_ID, PEDIDO, MUNICIPIO_ID ,TIPO_ELEMENTO_ID, TIPO_TRABAJO, DESC_TIPO_TRABAJO, FECHA_INGRESO, FECHA_CITA, PRODUCTO_ID, PRODUCTO, CONCEPTO_ID, CONCEPTO_ANTERIOR, DIRECCION_SERVICIO, PAGINA_SERVICIO, TECNOLOGIA_ID, FUENTE, FECHA_ESTADO, UEN_CALCULADA, ESTRATO, FECHA_CARGA, ESTADO_BLOQUEO, USUARIO_BLOQUEO_FENIX, ACTIVIDAD, GRUPO, CLIENTE_ID, DEPARTAMENTO,STATUS,OBSERVACIONES)
-            values ('".$pedido1['pedido']."','".$pedido1['pedido']."','".$pedido1['ciudad']."','NULL','NULL','NULL','".$pedido1['fecha_inicio']."','9999-01-01','NULL','NULL','MANUAL','MANUAL','NULL','NULL','NULL','".$pedido1['fuente']."','".$pedido1['fecha_inicio']."','HG','NULL','".$pedido1['fecha_inicio']."','N','','".$pedido1['actividad']."','ASIGNACIONES', 'NULL','".$pedido1['departamento']."','CERRADO_PETEC', '".$pedido1['observacion']."')";
+            $sqlInsertIngresos="insert into informe_petec_pendientesm (PEDIDO_ID, PEDIDO, MUNICIPIO_ID ,TIPO_ELEMENTO_ID, TIPO_TRABAJO, DESC_TIPO_TRABAJO, FECHA_INGRESO, FECHA_CITA, PRODUCTO_ID, PRODUCTO, CONCEPTO_ID, CONCEPTO_ANTERIOR, DIRECCION_SERVICIO, PAGINA_SERVICIO, TECNOLOGIA_ID, FUENTE, FECHA_ESTADO, UEN_CALCULADA, ESTRATO, FECHA_CARGA, ESTADO_BLOQUEO, USUARIO_BLOQUEO_FENIX, ACTIVIDAD, GRUPO, CLIENTE_ID, DEPARTAMENTO,STATUS,OBSERVACIONES,ASESOR)
+            values ('".$pedido1['pedido']."','".$pedido1['pedido']."','".$pedido1['ciudad']."','NULL','NULL','NULL','".$pedido1['fecha_inicio']."','9999-01-01','NULL','NULL','MANUAL','MANUAL','NULL','NULL','NULL','".$pedido1['fuente']."','".$pedido1['fecha_inicio']."','HG','NULL','".$pedido1['fecha_inicio']."','N','','".$pedido1['actividad']."','ASIGNACIONES', 'NULL','".$pedido1['departamento']."','CERRADO_PETEC', '".$pedido1['observacion']."','$usuarioGalleta')";
 
-            //echo $sqlInsertIngresos;
+            //echo $sqlInsertIngresos;/
             $r = $this->mysqli->query($sqlInsertIngresos) or die($this->mysqli->error.__LINE__);
             //Fix insertMPedido: se agregan campos y se habilita para insertar.
             //hago la actualizacion en fenix
@@ -19431,8 +19431,8 @@ public function pp(&$var){
             $insertGestion = $this->mysqli->query($queryGestion);
 
 
-            $insertIngresos="insert into informe_petec_pendientesm (PEDIDO_ID,PEDIDO,MUNICIPIO_ID,TIPO_ELEMENTO_ID,TIPO_TRABAJO,DESC_TIPO_TRABAJO,FECHA_INGRESO,FECHA_CITA,PRODUCTO_ID,PRODUCTO,CONCEPTO_ID,CONCEPTO_ANTERIOR,DIRECCION_SERVICIO,PAGINA_SERVICIO,TECNOLOGIA_ID,FUENTE,FECHA_ESTADO,UEN_CALCULADA,ESTRATO,FECHA_CARGA,ESTADO_BLOQUEO,USUARIO_BLOQUEO_FENIX,ACTIVIDAD,GRUPO,CLIENTE_ID,DEPARTAMENTO,STATUS)
- values ('".$gestion['gestion']['pedido']."','".$gestion['gestion']['pedido']."','".$gestion['gestion']['MUNICIPIO_ID']."','NULL','NULL','NULL','".$gestion['gestion']['fecha_inicio']."','9999-01-01','NULL','NULL','".$gestion['gestion']['CONCEPTO_ANTERIOR']."','MANUAL','NULL','NULL','NULL','".$gestion['gestion']['fuente']."','".$gestion['gestion']['fecha_inicio']."','HG','NULL','".$gestion['gestion']['fecha_inicio']."','N','','".$gestion['gestion']['actividad']."','ASIGNACIONES', 'NULL','".$gestion['gestion']['DEPARTAMENTO']."','CERRADO_PETEC')";
+            $insertIngresos="insert into informe_petec_pendientesm (PEDIDO_ID,PEDIDO,MUNICIPIO_ID,TIPO_ELEMENTO_ID,TIPO_TRABAJO,DESC_TIPO_TRABAJO,FECHA_INGRESO,FECHA_CITA,PRODUCTO_ID,PRODUCTO,CONCEPTO_ID,CONCEPTO_ANTERIOR,DIRECCION_SERVICIO,PAGINA_SERVICIO,TECNOLOGIA_ID,FUENTE,FECHA_ESTADO,UEN_CALCULADA,ESTRATO,FECHA_CARGA,ESTADO_BLOQUEO,USUARIO_BLOQUEO_FENIX,ACTIVIDAD,GRUPO,CLIENTE_ID,DEPARTAMENTO,STATUS,ASESOR)
+ values ('".$gestion['gestion']['pedido']."','".$gestion['gestion']['pedido']."','".$gestion['gestion']['MUNICIPIO_ID']."','NULL','NULL','NULL','".$gestion['gestion']['fecha_inicio']."','9999-01-01','NULL','NULL','".$gestion['gestion']['CONCEPTO_ANTERIOR']."','MANUAL','NULL','NULL','NULL','".$gestion['gestion']['fuente']."','".$gestion['gestion']['fecha_inicio']."','HG','NULL','".$gestion['gestion']['fecha_inicio']."','N','','".$gestion['gestion']['actividad']."','ASIGNACIONES', 'NULL','".$gestion['gestion']['DEPARTAMENTO']."','CERRADO_PETEC','$usuarioGalleta')";
 
 
         if($gestion['gestion']['source']=="MANUAL"){
