74544ea1b3d6930af30252d78fd5d72d2a45987c
Juan Jose Martinez Jimenez <Juan.J.Martinez@asesor.une.com.co>
2018-03-16 10:09:35 -0500

    geo

--- app/app.js
+++ app/app.js
@@ -737,7 +737,7 @@ app.factory("services", ['$http', '$timeout', function ($http) {
 		return $http.get(serviceBase + 'listadoTransaccionesCR');
 	};
 
-    obj.getLocalidadGeo = function (LOCALIDAD) {
+    obj.getLocalidadGeo = function () {
 
       return  $http.get(serviceBase + 'ListLocalidadGeo' + LOCALIDAD);
     };
