23f669469c6b1a597f62e75a5a8ac206941ebf86
Juan Jose Martinez Jimenez <Juan.J.Martinez@asesor.une.com.co>
2018-03-26 15:34:30 -0500

    geo

--- services/api.php
+++ services/api.php
@@ -11448,7 +11448,7 @@ private function demePedidoEdatel(){
 
         $query="SELECT LOCALIDAD FROM tbl_georreferenciados";
 
-       echo $query;
+       //echo $query;
         $r = $this->mysqli->query($query) or die($this->mysqli->error.__LINE__);
 
         if($r->num_rows > 0){
