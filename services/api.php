159f7378492f4838eaf091693311e9c20f8c3011
Mauricio Acevedo <mauricio.acevedo@une.com.co>
2018-04-09 12:56:05 -0500

    www

--- services/api.php
+++ services/api.php
@@ -8479,7 +8479,7 @@ private function getAgentScore($user){
         $param = $this->_request['parametro'];
         $value = $this->_request['valor'];
         $user = $this->_request['user'];
-        $orden = $this->_request['ORDEN'];
+        $orden = $this->_request['valor'];
 
         $sql="UPDATE gestor_parametros ".
             " SET VALOR='$value', USUARIO_ID='$user', ORDEN='$orden' where VARIABLE='$param'";
