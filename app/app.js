ccc900b719aa9b43b26c8c20ca975d6ed67fab33
Mauricio Acevedo <mauricio.acevedo@une.com.co>
2018-04-09 09:48:38 -0500

    WWW

--- app/app.js
+++ app/app.js
@@ -3392,6 +3392,7 @@ app.controller('IndicadoresCtrl', function ($scope, $rootScope, $location, $rout
 				$scope.ordenEntregaPedido = data.data['VALOR'];
 			}
 
+
             console.log($scope.ordenEntregaPedido);
 			return data.data;
 		});
