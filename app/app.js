f78b713f589e870b2b041f0cb0b342b2a45dcd02
Mauricio Acevedo <mauricio.acevedo@une.com.co>
2018-04-10 09:00:01 -0500

    WWW

--- app/app.js
+++ app/app.js
@@ -3377,6 +3377,8 @@ app.controller('IndicadoresCtrl', function ($scope, $rootScope, $location, $rout
 	   services.updateParametro(parametro, valor, $rootScope.logedUser.login).then(function (data) {
 			if ( parametro == "FECHA_ORDEN_DEMEPEDIDO" || parametro == "ORDEN_ENTREGA_PEDIDO") {
 				$scope.ordenEntregaPedido = valor;
+                $scope.ordenamientoDemepedidoNuevo = valor;
+
 				var date1 = new Date();
 				var year = date1.getFullYear();
 				var month = $scope.doubleDigit(date1.getMonth() + 1);
