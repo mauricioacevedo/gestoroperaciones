441a2cd0ecedd526851bbef8d458ce8ee78383da
Mauricio Acevedo <mauricio.acevedo@une.com.co>
2018-04-17 11:15:57 -0500

    WWW

--- services/api.php
+++ services/api.php
@@ -18169,6 +18169,86 @@ public function pp(&$var){
     }//----------------------------------------------- Historico de Pedidos
 
 
+    //--------------------EXPORTE PEDIDOS MALOS---------------------------
+    private function csvPedidosMalos(){
+
+        if($this->get_request_method() != "POST"){
+            $this->response('',406);
+        }
+
+
+
+
+        $params = json_decode(file_get_contents('php://input'),true);
+        $usuarioIp=$_SERVER['REMOTE_ADDR'];
+        $usuarioPc=gethostbyaddr($usuarioIp);
+        $galleta=json_decode(stripslashes($_COOKIE['logedUser']),true);
+        $galleta=stripslashes($_COOKIE['logedUser']);
+        $galleta= json_decode($galleta);
+        $galleta = json_decode(json_encode($galleta), True);
+        $usuarioid=$galleta['USUARIO_ID'];
+
+        //echo
+
+        $today = date("Y-m-d");
+        $ano=date("Y");
+
+        $filename="PEDIDOSMALOS_$today.csv";
+
+
+        $query="select pendiMalos.PEDIDO_ID,gestiMalos.user, gestiMalos.concepto_anterior, ".
+                " gestiMalos.estado, gestiMalos.motivo_malo, gestiMalos.fecha_fin, ".
+                " CAST(TIMEDIFF(gestiMalos.fecha_fin,NOW()) AS CHAR (255)) AS ANSSOLUCION "
+                " from informe_petec_pendientesm pendiMalos ".
+                " inner join pedidos gestiMalos ".
+                " on pendiMalos.STATUS = gestiMalos.ESTADO_ID AND pendiMalos.PEDIDO_ID = gestiMalos.pedido_id ".
+                " where pendiMalos.STATUS = ('MALO') order by gestiMalos.fecha_fin desc";
+        //echo $query;
+
+        $rst = $this->mysqli->query($query) or die($this->mysqli->error.__LINE__);
+
+        if($rst->num_rows > 0){
+
+            /*Insert en log
+								$sql_log="insert into emtelco.re_logoperaciones (USUARIO_ID, TIPO_ACTIVIDAD, DESCRIPCION, IDENTIFICADOR, IP, PC) values(UPPER('$usuarioid'),'EXPORTE','EXPORTO CSV_USUARIOS','LOGIN: $usuarioid','$usuarioIp','$usuarioPc')";
+
+								$rlog = $this->connemtel->query($sql_log) or die($this->connemtel->error.__LINE__);
+								//Insert en log*/
+
+            $result = array();
+            $fp = fopen("../tmp/$filename", 'w');
+            //echo $fp;
+            $columnas=array( 'PEDIDO_ID',
+                'USUARIO',
+                'CONCEPTO_ANTERIOR',
+                'MOTIVO_MALO',
+                'FECHA_FIN'
+            );
+
+            fputcsv($fp, $columnas,',');
+            //$carlitos=0;
+            while($row = $rst->fetch_assoc()){
+
+                //$row['OBSERVACIONES']=utf8_decode($row['OBSERVACIONES']);
+                //$result[] = $row;
+                fputcsv($fp, $row);
+                //if($carlitos==0){var_dump($row);$carlitos=1;};
+            }
+
+            fclose($fp);
+
+            $this->response($this->json(array($filename)), 200);
+
+
+        }
+
+
+        $this->response('',203);        // If no records "No Content" status
+
+    }
+
+    //--------------------EXPORTE PEDIDOS MALOS FIN---------------------------
+
     private function csvUsuarios(){
 
         if($this->get_request_method() != "POST"){
