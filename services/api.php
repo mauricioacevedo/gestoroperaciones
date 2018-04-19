20fed4b1d4a5ad14204d96c8db9637433f30f8c1
Mauricio Acevedo <mauricio.acevedo@une.com.co>
2018-04-19 14:45:06 -0500

    www

--- services/api.php
+++ services/api.php
@@ -18740,7 +18740,7 @@ public function pp(&$var){
         $grupo 	    = $params['grupo'];
         $actividad  = $params['actividad'];
         $today		= date("Y-m-d");
-        echo $params;
+        echo var_dump($params);
 
 
 
