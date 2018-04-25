32652b00ff5a9bb1292d93d33799296a377b238d
Mauricio Acevedo <mauricio.acevedo@une.com.co>
2018-04-25 14:49:36 -0500

    www

--- app/app.js
+++ app/app.js
@@ -4939,7 +4939,7 @@ app.controller('PedidosMalosCtrl', function ($scope, $rootScope, $location, $rou
 	$scope.ignorarPedido = function (index) {
         var loader = document.getElementById("class"+index);
 
-        //$scope.listaPedidosMalos={}
+        $scope.listaPedidosMalos={};
 
         angular.copy($scope.listaPedidosMalos[index], $scope.PEDIDO_ID);
 
