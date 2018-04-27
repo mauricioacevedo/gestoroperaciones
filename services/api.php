93d8e3936bd53a5a20aaef15296a47ea978db7fb
Mauricio Acevedo <mauricio.acevedo@tigoune.com.co>
2018-04-27 10:26:21 -0500

    fix csvpendietnes

--- services/api.php
+++ services/api.php
@@ -6215,7 +6215,7 @@ private function getAgentScore($user){
             //" CASE WHEN a.TIPO_TRABAJO IN ('NA NUEVO','NUEVO') then 'NUEVO' ELSE 'CAMBIO' END AS TIPO_TRABAJO, ".
             " CASE WHEN UPPER(a.TIPO_TRABAJO) like '%NUEVO%' OR UPPER(a.TIPO_TRABAJO) LIKE '%TRASL%' OR UPPER(a.TIPO_TRABAJO)='CAMBIO DE DOMICILIO' THEN 'NUEVO' ".
             " else 'CAMBIO' ".
-            " end as TIPO_TRABAJO ".
+            " end as TIPO_TRABAJO, ".
             " a.TIPO_TRABAJO AS TIPO_TRABAJO_ORIGINAL, ".
             " a.TIPO_ELEMENTO_ID, ".
             " a.PRODUCTO, ".
