8f56ba33e845d1a66090e357878eea39b5052576
Mauricio Acevedo <mauricio.acevedo@une.com.co>
2018-05-07 12:31:51 -0500

    www

--- services/api.php
+++ services/api.php
@@ -1501,9 +1501,10 @@ class API extends REST {
             //echo $sqlInsertIngresos;/
             $r = $this->mysqli->query($sqlInsertIngresos) or die($this->mysqli->error.__LINE__);
 
-            $sqlCerrarManuales = "update informe_petec_pendientesm set STATUS = 'CERRADO_PETEC' where PEDIDO_ID = $pedidoid ".
+            $sqlCerrarManuales = "update informe_petec_pendientesm set STATUS = 'CERRADO_PETEC' where PEDIDO_ID = '$pedidoid' ".
             " and STATUS = 'CERRADO_PETEC' ";
             $r = $this->mysqli->query($sqlCerrarManuales) or die($this->mysqli->error.__LINE__);
+            echo $sqlCerrarManuales;
 
             //Fix insertMPedido: se agregan campos y se habilita para insertar.
             //hago la actualizacion en fenix
