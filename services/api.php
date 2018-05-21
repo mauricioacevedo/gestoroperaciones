5c0d0e7b6dae404192807af38e44d1be2abae383
Mauricio Acevedo <mauricio.acevedo@une.com.co>
2018-05-21 11:51:28 -0500

    www

--- services/api.php
+++ services/api.php
@@ -18271,7 +18271,7 @@ public function pp(&$var){
         }
 
         $concepto = $this->_request['concepto'];
-        echo $concepto;
+        echo var_dump($concepto);
 
         $query=	" select ZONA, count(ZONA) AS TOTAL from informe_petec_pendientesm ".
             "where STATUS = 'PENDI_PETEC' and ZONA not in ('NULL') AND CONCEPTO_ID = '14' group by ZONA order by TOTAL desc ";
