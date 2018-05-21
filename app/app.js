b666bfec3d1a68d071cf9a23e7be6b79096f3c8b
Mauricio Acevedo <mauricio.acevedo@une.com.co>
2018-05-21 12:01:37 -0500

    www

--- app/app.js
+++ app/app.js
@@ -17824,7 +17824,7 @@ app.controller('gestionAsignacionesCtrl', function ($scope, $rootScope, $locatio
     $rootScope.getConceptosGestor();						// Inicializo la variable Global para los conceptos.
 	/*$rootScope.getZonasGestor();*/
 
-    $rootScope.getZonasGestor = function (iconcepto) {
+    $rootScope.getZonasGestor = function () {
         services.getGestorZonas(iconcepto).then(
 			function (data) {
 
