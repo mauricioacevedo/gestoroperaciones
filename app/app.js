bb73d7ca9125fbdc2a7a3fa33350019cfcdd693e
Mauricio Acevedo <mauricio.acevedo@une.com.co>
2018-05-22 14:38:40 -0500

    www

--- app/app.js
+++ app/app.js
@@ -17836,7 +17836,7 @@ app.controller('gestionAsignacionesCtrl', function ($scope, $rootScope, $locatio
                 //console.log($rootScope.listadoZonas);
                 //$rootScope.errorDatos = "null";
 
-                if (data.data == {}){
+                if (data.data === {}){
                     $rootScope.errorDatos = "Este concepto no tiene pendientes";
                 }
                 return data.data;
