06b3b1063cad90c9876f62e85837c67de93a4694
Mauricio Acevedo <mauricio.acevedo@une.com.co>
2018-06-01 08:43:06 -0500

    www

--- services/api.php
+++ services/api.php
@@ -19929,6 +19929,7 @@ public function pp(&$var){
             $malingreso = 'NO';
         }
 
+
         $column_names = array('pedido', 'fuente', 'actividad', 'ESTADO_ID', 'OBSERVACIONES_PROCESO', 'estado', 'user','duracion','INCIDENTE','fecha_inicio','fecha_fin','PEDIDO_ID','SUBPEDIDO_ID','SOLICITUD_ID','MUNICIPIO_ID','CONCEPTO_ANTERIOR','idllamada','nuevopedido','motivo_malo','fecha_estado','concepto_final','source','fecha_cita','DEPARTAMENTO','TIPO_TRABAJO','TECNOLOGIA_ID');
         $keys = array_keys($gestion['gestion']);
 
