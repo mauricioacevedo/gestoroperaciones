93ebb3beea784de809b6252d52427f435d20a991
Mauricio Acevedo <mauricio.acevedo@une.com.co>
2018-06-05 10:04:30 -0500

    WWW

--- services/api.php
+++ services/api.php
@@ -1476,7 +1476,7 @@ class API extends REST {
                 //echo "ingreso";
         }
 
-        $column_names = array('pedido', 'fuente', 'actividad','estado', 'user','duracion','INCIDENTE','fecha_inicio','fecha_fin','concepto_final');
+        $column_names = array('pedido', 'fuente', 'actividad','estado','ESTADO_ID', 'user','duracion','INCIDENTE','fecha_inicio','fecha_fin','concepto_final');
         $keys = array_keys($pedido);
 
         //echo var_dump ($pedido);guardarPedidoGestionguardarPedidoGestion
