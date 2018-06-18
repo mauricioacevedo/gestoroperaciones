24c3db8dcabe656eb743e08d9c002e7287b93b5e
Mauricio Acevedo <mauricio.acevedo@une.com.co>
2018-06-18 17:14:20 -0500

    www

--- services/api.php
+++ services/api.php
@@ -15382,8 +15382,8 @@ public function pp(&$var){
              $rst = mysql_query ($sqlUltimoTurno);
                   if ($row = mysql_fetch_row($rst)) {
                       $TurnoActivo = trim($row[0]);
-                      echo var_dump ($TurnoActivo);
                       }
+            echo var_dump ($TurnoActivo);
 
         }
 
