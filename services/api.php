ad6285eda3473dba4be687c57d10651c4e7ea61a
Mauricio Acevedo <mauricio.acevedo@une.com.co>
2018-06-18 17:25:10 -0500

    www

--- services/api.php
+++ services/api.php
@@ -15376,13 +15376,14 @@ public function pp(&$var){
 
         if ($Novedad == 'SI')
         {
-             $sqlUltimoTurno = "select max(ID) from Tbl_Turnos where USUARIO = '$Login' and ESTADO = 'Activo' ";
+             //$sqlUltimoTurno = "select max(ID) from Tbl_Turnos where USUARIO = '$Login' and ESTADO = 'Activo' ";
 
-             $rst = $this->mysqli->query($sqlUltimoTurno) or die($this->mysqli->error.__LINE__);
-                  if ($row = mysql_fetch_row($rst)) {
-                      $TurnoActivo = trim($row[0]);
-                      echo var_dump ($TurnoActivo);
-                    }
+                  $sqlUltimoid = "select max(ID) from Tbl_Turnos where USUARIO = '$Login' and ESTADO = 'Activo'";
+                  $rst2 = mysql_query ($sqlUltimoid);
+                  if ($row = mysql_fetch_row($rst2)) {
+                      $Carga = trim($row[0]);
+                       echo "Ultimo".$Carga;
+                      }
 
         }
 
