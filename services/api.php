c03cd215327ef257c43175c1ee27767a524911c5
Mauricio Acevedo <mauricio.acevedo@une.com.co>
2018-06-25 11:34:51 -0500

    www

--- services/api.php
+++ services/api.php
@@ -15303,7 +15303,7 @@ public function pp(&$var){
             }
         }
 
-        $query="SELECT count(*) as novedades from Tbl_Novedad_Turnos0 ";
+        $query="SELECT count(*) as novedades from Tbl_Novedad_Turnos ";
         $r2 = $this->mysqli->query($query) or die($this->mysqli->error.__LINE__);
         if($r2->num_rows > 0){
             $novedades = array();
