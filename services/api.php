cb19c64097bc1653caaacec21d3118b9ac756f60
Mauricio Acevedo <mauricio.acevedo@une.com.co>
2018-06-25 12:54:41 -0500

    WWW

--- services/api.php
+++ services/api.php
@@ -15340,7 +15340,7 @@ public function pp(&$var){
                 " C.PROGRAMO, C.FECHACARGA ".
 	            " FROM tbl_usuarios A ".
 	            " inner join registro_ingreso_usuarios B on A.USUARIO_ID = B.usuario ".
-	            " left outer join Tbl_Turnos C on A.USUARIO_ID = C.USUARIO ".
+	            " RIGHT outer join Tbl_Turnos C on A.USUARIO_ID = C.USUARIO ".
 	            " where B.status = 'logged in' ".
 	            " and B.fecha_ingreso between '$fecha 00:00:00' and '$fecha 23:59:59' ".
 	            " and C.ESTADO not in ('CUMPLIDO') group by A.USUARIO_ID, C.ID";
