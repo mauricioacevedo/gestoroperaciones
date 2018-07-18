199e6cd3ff0308e6b4ffacc614bfaaf712186e68
Sergio Alexis Zapata Mejia <Sergio.Zapata@Asesor.une.com.co>
2018-07-18 11:32:55 -0500

    www

--- services/api.php
+++ services/api.php
@@ -9485,9 +9485,10 @@ private function getAgentScore($user){
         $zona           =   $this->_request['zona'];
 
         $fechaservidor  =   date('Y-m-d');
-        //$Fechamanana    =   date('Y-m-d', strtotime($fechaservidor'+ 1 days'));
+        $Diasiguiente    =   date('Y-m-d');
 
-        //echo var_dump ($fechamanana);
+
+        echo var_dump ($Diasiguiente);
         //echo var_dump ($plaza);
 
         if($zona==""||$zona=="null"||$zona=="undefined") $zona="TODOS";
