ac8c4d0ad0a3c7e5b1a9dd1cc34674a21b1c4a0d
Sergio Alexis Zapata Mejia <Sergio.Zapata@Asesor.une.com.co>
2018-07-19 12:43:32 -0500

    www

--- services/api.php
+++ services/api.php
@@ -9577,21 +9577,23 @@ private function getAgentScore($user){
 
             $Pendiente = "and b.FECHA_CITA not in ('9999-00-00','SIN AGENDA','') and b.FECHA_CITA >= '$Diasiguiente' order by b.FECHA_CITA, b.FECHA_INGRESO asc";
         }
-        else  ($TipoPendiente == "SIN AGENDA"){
+        else if ($TipoPendiente == "SIN AGENDA"){
 
             $Pendiente = "and b.FECHA_CITA in ('9999-00-00','SIN AGENDA','') and b.FECHA_CITA >= '$Diasiguiente' order by b.FECHA_CITA, b.FECHA_INGRESO asc";
         }
-/*        else if ($TipoPendiente == "B2B"){
+        else if ($TipoPendiente == "B2B"){
 
             $Pendiente = "and b.UEN_CALCULADA in ('B2B') and b.FECHA_CITA >= '$Diasiguiente' order by b.FECHA_CITA, b.FECHA_INGRESO asc";
 
         }
         else{
+            $Pendiente = "";
+        }
+        echo "xy";
 
-        }*/
+        return;
 
-
-        echo var_dump ($Pendiente);
+        //echo var_dump ($Pendiente);
         //echo var_dump ($plaza);
 
         $query1="select b.CLIENTE_ID,b.PEDIDO_ID,b.SUBPEDIDO_ID,b.SOLICITUD_ID,b.FECHA_ESTADO,b.FECHA_INGRESO,b.FECHA_CITA ".
