4f5c314ea9ff1ce29a56056f1224baa9ad86cb47
Sergio Alexis Zapata Mejia <Sergio.Zapata@Asesor.une.com.co>
2018-07-19 15:02:46 -0500

    www

--- services/api.php
+++ services/api.php
@@ -9575,16 +9575,16 @@ private function getAgentScore($user){
 
        if ($TipoPendiente == "CON AGENDA"){
 
-            $Pendiente = "and b.FECHA_CITA not in ('9999-00-00','SIN AGENDA','') and b.FECHA_CITA >= '$Diasiguiente' order by b.FECHA_CITA, b.FECHA_INGRESO asc";
+            $Pendiente = "and b.FECHA_CITA not in ('9999-00-00','SIN AGENDA','') and b.FECHA_CITA >= '$Diasiguiente' order by b.FECHA_CITA, b.FECHA_INGRESO asc"
         }
         else if ($TipoPendiente == "SIN AGENDA"){
 
-            $Pendiente = "and b.FECHA_CITA in ('9999-00-00','SIN AGENDA','') and b.FECHA_CITA >= '$Diasiguiente' order by b.FECHA_CITA, b.FECHA_INGRESO asc";
+            $Pendiente = "and b.FECHA_CITA in ('9999-00-00','SIN AGENDA','') and b.FECHA_CITA >= '$Diasiguiente' order by b.FECHA_CITA, b.FECHA_INGRESO asc"
         }
        else ($TipoPendiente == "B2B")
         {
 
-            $Pendiente = "and b.UEN_CALCULADA in ('B2B') and b.FECHA_CITA >= '$Diasiguiente' order by b.FECHA_CITA, b.FECHA_INGRESO asc";
+            $Pendiente = "and b.UEN_CALCULADA in ('B2B') and b.FECHA_CITA >= '$Diasiguiente' order by b.FECHA_CITA, b.FECHA_INGRESO asc"
 
         }
         //echo "xy";
@@ -9603,7 +9603,7 @@ private function getAgentScore($user){
             " and b.ASESOR ='' ".
             $tipo_trabajo.
             $concepto.
-            $Pendiente
+            $Pendiente;
             //$plaza.
             //$zona2.
             //" and b.CONCEPTO_ID='$concepto' ".
