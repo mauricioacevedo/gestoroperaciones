a765a65e4186cd0bc9b3959c10d7b010641fd84e
Jhor Maykol Bedoya Gomez <jhor.Bedoya@asesor.une.com.co>
2018-07-25 11:34:22 -0500

    www

--- services/api.php
+++ services/api.php
@@ -9596,7 +9596,7 @@ private function getAgentScore($user){
             //" and b.CONCEPTO_ID='$concepto' ".
             //" AND b.MUNICIPIO_ID IN (select a.MUNICIPIO_ID from tbl_plazas a where a.PLAZA='$plaza') ".
             //" order by b.$parametroBusqueda2 $parametroOrden ";
-        //echo var_dump($query1);
+        echo var_dump($query1);
 
 
 
