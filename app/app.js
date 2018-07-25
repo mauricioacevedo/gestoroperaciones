b1b24acde46a9ac2e8dd163ef516056f9788a893
Sergio Alexis Zapata Mejia <Sergio.Zapata@Asesor.une.com.co>
2018-07-25 12:41:36 -0500

    www

--- app/app.js
+++ app/app.js
@@ -16242,7 +16242,7 @@ app.controller('AsignacionesCtrl', function ($scope, $rootScope, $location, $rou
     $scope.deme_pedidos = [{PEDIDO_ID:" NUEVO "}];
     $scope.agentScore="-1";
     $scope.TipoPendiente="CON AGENDA";
-//    $scope.izona="TODAS";
+    $scope.iplaza="TODOS";
 
 	var pedidos = services.getPedidosUser(userID).then(function (data) {
 		$scope.pedidos = data.data[0];
