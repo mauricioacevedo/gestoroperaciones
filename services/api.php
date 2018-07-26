e50225e507b5796c36e00cac5706cab271015d69
Jhor Maykol Bedoya Gomez <jhor.Bedoya@asesor.une.com.co>
2018-07-26 07:18:11 -0500

    www

--- services/api.php
+++ services/api.php
@@ -474,7 +474,7 @@ class API extends REST {
         $today = date("Y-m-d h:i:s");
         $filename="Agendamiento-Fenix_NAL-$login-$today.csv";
 
-        $query=" (SELECT ".
+        $query=" SELECT ".
             " a.PEDIDO_ID,a.CONCEPTOS,a.ACTIVIDADES,a.NOVEDAD,a.FECHA_CITA_FENIX ".
             " ,a.FECHA_CITA_REAGENDA,a.JORNADA_CITA ".
             " ,a.FECHA_INGRESO,a.FECHA_CARGA,a.ASESOR,a.FECHA_INICIO,a.FECHA_FIN ".
@@ -484,7 +484,7 @@ class API extends REST {
             " ,a.DIRECCION_ENVIO,a.E_MAIL_AVISAR,a.MICROZONA,a.NOMBRE_USUARIO ".
             " ,a.PARENT_ID,a.TELEFONO_AVISAR,a.TIEMPO_TOTAL ".
             " ,a.PROGRAMACION,a.SOURCE,a.DEPARTAMENTO,a.ACCESO,a.NUMERO_CR ".
-            " ,a.IDLLAMADA,a.SUBZONA_ID,a.PROCESO,a.OBSERVACION_GESTOR".
+            " ,a.IDLLAMADA,a.SUBZONA_ID,a.PROCESO,a.OBSERVACION_GESTOR ".
             " , (SELECT hr.TODAY_TRIES FROM gestor_pendientes_reagendamiento hr WHERE hr.ID = (SELECT MAX( b.id )  ".
             "      FROM gestor_pendientes_reagendamiento b ".
             "   WHERE b.PEDIDO_ID =  a.PEDIDO_ID) )AS INTENTOS_CONTACTO ".
