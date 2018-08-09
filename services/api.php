f4c6e2084b57c89e0eaa2a4f8aa79da675d40967
Sergio Alexis Zapata Mejia <Sergio.Zapata@Asesor.une.com.co>
2018-08-09 07:01:00 -0500

    www

--- services/api.php
+++ services/api.php
@@ -9488,7 +9488,7 @@ private function getAgentScore($user){
         $TipoPendiente  =   $this->_request['TipoPendiente'];
 		$Municipioid	=	$this->_request['iMunicipios'];
 
-        echo var_dump($TipoPendiente);
+        //echo var_dump($TipoPendiente);
 
         $fechaservidor  =   date('Y-m-d');
         $Diasiguiente   =   date('Y-m-d',strtotime($fechaservidor."+ 1 days"));
@@ -9595,7 +9595,7 @@ private function getAgentScore($user){
             " where b.STATUS='$STATUS'  ".
             " and b.ASESOR ='' ".
 			" and b.FUENTE='OPEN_PEREIRA' ".
-			//" and MUNICIPIO_ID = '$Municipioid' "
+			" and MUNICIPIO_ID = '$Municipioid' "
             //$tipo_trabajo.
             //$concepto.
             " $Pendiente ".
@@ -9605,7 +9605,7 @@ private function getAgentScore($user){
             //" AND b.MUNICIPIO_ID IN (select a.MUNICIPIO_ID from tbl_plazas a where a.PLAZA='$plaza') ".
             //" order by b.$parametroBusqueda2
 			" $parametroOrden ";
-        //echo var_dump($query1);
+        echo var_dump($query1);
 
 
 
