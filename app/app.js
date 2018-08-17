5111a7551f0eee835e032841cbabdb1f9d8646ec
Bryan Alejandro Arango Villa <barangov@epmtelco.com.co>
2018-08-17 13:06:38 -0500

    Se gestiona el Puntaje por agente

--- app/app.js
+++ app/app.js
@@ -18594,10 +18594,10 @@ app.controller('gestionAsignacionesCtrl', function ($scope, $rootScope, $locatio
 
     $scope.getAgentColor = function(agentScore){
 
-        if(agentScore<160) return "red";
+        if(agentScore<50) return "red";
 
         //if(agentScore<115) return "#f0ad4e";
-        if(agentScore<200) return "orange";
+        if(agentScore<100) return "orange";
 
         //return "#5cb85cy";
         return "green";
