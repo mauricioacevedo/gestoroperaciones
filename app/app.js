de8e0d7cd3145988613e6923f3052d77423935f8
Bryan Alejandro Arango Villa <Bryan.Arango@asesor.une.com.co>
2018-09-11 14:24:25 -0500

    Se modifica los Permisos de Usuarios

--- app/app.js
+++ app/app.js
@@ -3474,7 +3474,6 @@ app.controller('IndicadoresCtrl', function ($scope, $rootScope, $location, $rout
                 alert("Si tienes permisos para este sitio");
             }
     };
-    console.log(usuario);
 
 	$scope.doubleDigit = function (num) {
 
