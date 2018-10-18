6fb176ef84b0b521d5a4e9687640e2119c0a072d
Bryan Alejandro Arango Villa <Bryan.Arango@asesor.une.com.co>
2018-10-18 10:35:58 -0500

    Gestion para registrar la forma manual de RC-SIEBEL

--- services/api.php
+++ services/api.php
@@ -1616,9 +1616,9 @@ class API extends REST {
 				$sqlInsertIngresos="insert into informe_petec_pendientesm (PEDIDO_ID, PEDIDO, MUNICIPIO_ID ,TIPO_ELEMENTO_ID, TIPO_TRABAJO, DESC_TIPO_TRABAJO, FECHA_INGRESO, FECHA_CITA, PRODUCTO_ID, PRODUCTO, CONCEPTO_ID, CONCEPTO_ANTERIOR, DIRECCION_SERVICIO, PAGINA_SERVICIO, TECNOLOGIA_ID, FUENTE, FECHA_ESTADO, UEN_CALCULADA, ESTRATO, FECHA_CARGA, ESTADO_BLOQUEO, USUARIO_BLOQUEO_FENIX, ACTIVIDAD, GRUPO, CLIENTE_ID, DEPARTAMENTO,STATUS,OBSERVACIONES,ASESOR)
             	values ('".$pedido1['pedido']."','".$pedido1['pedido']."','".$pedido1['ciudad']."','NULL','NULL','NULL','".$pedido1['fecha_inicio']."','9999-01-01','NULL','NULL','MANUAL','MANUAL','NULL','NULL','NULL','".$pedido1['fuente']."','".$pedido1['fecha_inicio']."','HG','NULL','".$pedido1['fecha_inicio']."','N','','".$pedido1['actividad']."','ASIGNACIONES', 'NULL','".$pedido1['departamento']."','CERRADO_PETEC', '".$pedido1['observacion']."','$usuarioGalleta')";
 
-				$r = $this->mysqli->query($sqlInsertIngresos) or die($this->mysqli->error.__LINE__);*/
+				$r = $this->mysqli->query($sqlInsertIngresos) or die($this->mysqli->error.__LINE__);
 
-				}
+				}*/
 
 
 
@@ -1679,7 +1679,7 @@ class API extends REST {
             // ---------------------------------- SQL Feed
             //echo ("ingresooooooo");
             //$this->response(json_encode(array("msg"=>"N/A","data" => $today,"agent_score"=>$agentScore)),200);
-		//}
+		}
 
 
 
